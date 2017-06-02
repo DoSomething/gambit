9d4bb56c1be38958533a81aacd1840fe443d7a45
Aaron Schachter <aaronschachter@gmail.com>
2017-06-02 14:40:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# slothie
+
+Slothie is a bot built with Node and Rivescript.
+
+## Getting started
+
+* Clone this repo
+* `npm install`
+* `npm start`
+* Send a message to Slothie in the terminal
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const readline = require("readline");
+const fs = require("fs");
+const RiveScript = require("rivescript");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let bot = null;
+function loadBot() {
+  bot = new RiveScript({
+    concat:  "newline",
+  });
+  bot.ready = false;
+  bot.loadDirectory('brain', loadingDone, loadingError);
+}
+loadBot();
+
+console.log('Send a message to Slothie B\n\n');
+
+rl.setPrompt("You> ");
+rl.prompt();
+rl.on('line', function(cmd) {
+  // Handle commands.
+  if (cmd === "/help") {
+    help();
+  } else if (cmd.indexOf("/data") === 0) {
+    console.log(bot.getUservars("localuser"));
+  } else if (cmd.indexOf("/eval ") === 0) {
+    console.log(eval(cmd.replace("/eval ", "")));
+  } else if (cmd.indexOf("/log ") === 0) {
+    console.log(eval(cmd.replace("/log ", "")));
+  } else if (cmd === "/quit") {
+    process.exit(0);
+  } else {
+    // Get a reply from the bot.
+    var reply = (bot && bot.ready)
+      ? bot.reply("localuser", cmd)
+      : "ERR: Bot Not Ready Yet";
+    console.log("Bot>", reply);
+  }
+  
+  rl.prompt();
+}).on('close', function() {
+  console.log("");
+  process.exit(0);
+});
+
+
+function loadingDone(batchNumber) {
+  bot.sortReplies();
+  bot.ready = true;
+}
+
+function loadingError(error, batchNumber) {
+  console.error("Loading error: " + error);
+}
+
+function help() {
+  console.log("Supported commands:");
+  console.log("/help        : Show this text.");
+  console.log("/eval <code> : Evaluate JavaScript code.");
+  console.log("/log <code>  : Shortcut to /eval console.log(code).");
+  console.log("/quit        : Exit the program.");
+}
