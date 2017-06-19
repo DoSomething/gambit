3e67f51dfe562b1330c053fd00cbdf0cd8d968c1
Aaron Schachter <aaronschachter@gmail.com>
2017-06-18 20:53:52 -0700

    Adds middleware for Michael catchall reply

--- app/routes/chatbot.js
+++ app/routes/chatbot.js
@@ -9,13 +9,15 @@ bot.getBot();
 const lookUpUserMiddleware = require('../../lib/middleware/user-get');
 const setBotUservarsMiddleware = require('../../lib/middleware/bot-set-uservars');
 const getBotReplyMiddleware = require('../../lib/middleware/bot-get-reply');
-const botRivescriptReplyMiddleware = require('../../lib/middleware/bot-reply-rivescript');
+const botReplyRivescriptMiddleware = require('../../lib/middleware/bot-reply-rivescript');
+const botReplyMichaelMiddleware = require('../../lib/middleware/bot-reply-michael');
 
 router.use(lookUpUserMiddleware());
 router.use(setBotUservarsMiddleware());
 router.use(getBotReplyMiddleware());
 router.use(getBotReplyMiddleware());
-router.use(botRivescriptReplyMiddleware());
+router.use(botReplyRivescriptMiddleware());
+router.use(botReplyMichaelMiddleware());
 
 router.post('/', (req, res) => {
   return res.send({
