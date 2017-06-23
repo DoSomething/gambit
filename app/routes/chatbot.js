36ece0bc9adf8152303c934c021a1d138934019a
Aaron Schachter <aaronschachter@gmail.com>
2017-06-23 11:25:40 -0700

    Adds default reply middleware

--- app/routes/chatbot.js
+++ app/routes/chatbot.js
@@ -11,24 +11,30 @@ const getUserMiddleware = require('../../lib/middleware/user-get');
 const createUserMiddleware = require('../../lib/middleware/user-create');
 const createInboundMessageMiddleware = require('../../lib/middleware/message-inbound-create');
 const createOutboundMessageMiddleware = require('../../lib/middleware/message-outbound-create');
+
 const getBotReplyMiddleware = require('../../lib/middleware/bot-get-reply');
 const brainReplyMiddleware = require('../../lib/middleware/reply-brain');
+const defaultReplyMiddleware = require('../../lib/middleware/reply-default')
 const noReplyMiddleware = require('../../lib/middleware/reply-noreply');
 const michaelTopicMiddleware = require('../../lib/middleware/reply-michael');
 const botReplySignupKeywordMiddleware = require('../../lib/middleware/bot-reply-signup-keyword');
 const botReplySignupMenuMiddleware = require('../../lib/middleware/bot-reply-signup-menu');
 const botReplySignupContinueMiddleware = require('../../lib/middleware/bot-reply-signup-continue');
 
+// Load user.
 router.use(getUserMiddleware());
 router.use(createUserMiddleware());
 router.use(createInboundMessageMiddleware());
+// Get bot response to user message.
 router.use(getBotReplyMiddleware());
+// Parse response.
 router.use(brainReplyMiddleware());
 router.use(noReplyMiddleware());
 router.use(michaelTopicMiddleware());
 // router.use(botReplySignupKeywordMiddleware());
 // router.use(botReplySignupMenuMiddleware());
 // router.use(botReplySignupContinueMiddleware());
+router.use(defaultReplyMiddleware());
 router.use(createOutboundMessageMiddleware());
 
 router.post('/', (req, res) => helpers.sendChatbotResponse(req, res));
