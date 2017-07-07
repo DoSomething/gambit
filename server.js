aa519560c5e427eb898e48be900bb00efeb58a4b
Aaron Schachter <aaronschachter@gmail.com>
2017-07-07 10:15:26 -0700

    Enable CORS

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const mongoose = require('mongoose');
 const restify = require('express-restify-mongoose');
 const config = require('./config');
 
+app.use((req, res, next) => {
+  if (! config.corsEnabled) {
+    return next();
+  }
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  return next();
+});
+
 mongoose.connect(config.dbUri);
 mongoose.Promise = global.Promise;
 
