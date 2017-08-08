875127cc7401334ef8b3aa7765586c28637b283c
Aaron Schachter <aaronschachter@gmail.com>
2017-08-08 16:59:26 -0700

    WIP - Starts on sending Campaign messages

--- app/routes/send-message.js
+++ app/routes/send-message.js
@@ -5,14 +5,16 @@ const helpers = require('../../lib/helpers');
 
 const router = express.Router();
 
-const paramsMiddleware = require('../../lib/middleware/send-message/params');
+const campaignMiddleware = require('../../lib/middleware/send-message/campaign');
+const supportMiddleware = require('../../lib/middleware/send-message/params');
 const getConversationMiddleware = require('../../lib/middleware/conversation-get');
 const supportResolvedMiddleware = require('../../lib/middleware/send-message/support-resolved');
 const outboundMessageMiddleware = require('../../lib/middleware/send-message/message-outbound');
 
-router.use(paramsMiddleware());
 router.use(getConversationMiddleware());
-router.use(supportResolvedMiddleware());
+router.use(campaignMiddleware());
+//router.use(supportMiddleware());
+//router.use(supportResolvedMiddleware());
 router.use(outboundMessageMiddleware());
 router.post('/', (req, res) => helpers.sendResponse(req, res));
 
