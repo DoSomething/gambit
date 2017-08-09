0878e7f2fbc02890f3abe574405d801a83536854
Aaron Schachter <aaronschachter@gmail.com>
2017-08-09 14:20:23 -0700

    Adds Twilio config, save Conversation.medium as 'sms'

---
+++ config/lib/twilio.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  fromNumber: process.env.TWILIO_FROM_NUMBER,
+  messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+};
