f0e7bcccdaa0ad2bf538661f68a1fc48647e92a6
Rafa <rapala61@gmail.com>
2017-08-22 18:14:30 -0400

    added instructions for photo command in Consolebot intro.txt

--- config/lib/consolebot/index.js
+++ config/lib/consolebot/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 require('dotenv').config();
 const appConfig = require('../../');
 
+// Used as a Reportback image.
+const photoUrl = 'https://www.wired.com/wp-content/uploads/2015/03/The-X-Files1-1024x768.jpg';
 const basicAuthString = `${process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_NAME || 'puppet'}:${process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_PASS || 'totallysecret'}`;
 
 const configVars = {
@@ -15,6 +17,7 @@ const configVars = {
   replyPrefix: process.env.DS_CONSOLEBOT_REPLY_PREFIX || 'Bot>',
   replyColor: process.env.DS_CONSOLEBOT_REPLY_COLOR || 'magenta',
   url: `http://${basicAuthString}@localhost:${appConfig.port}/api/v1/receive-message`,
+  photoUrl: process.env.DS_CONSOLEBOT_PHOTO_URL || photoUrl,
 };
 
 module.exports = configVars;
