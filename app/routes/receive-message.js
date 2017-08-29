ea7400cbf17720bea625dc6a749354625a58f829
Aaron Schachter <aaronschachter@gmail.com>
2017-08-28 18:03:39 -0700

    Remove deprecated middleware

--- app/routes/receive-message.js
+++ app/routes/receive-message.js
@@ -24,11 +24,8 @@ const currentCampaignMiddleware = require('../../lib/middleware/receive-message/
 const closedCampaignMiddleware = require('../../lib/middleware/receive-message/campaign-closed');
 const parseAskSignupMiddleware = require('../../lib/middleware/receive-message/parse-ask-signup-answer');
 const parseAskContinueMiddleware = require('../../lib/middleware/receive-message/parse-ask-continue-answer');
-const sendAskContinueMiddleware = require('../../lib/middleware/receive-message/send-ask-continue');
-const setCampaignMiddleware = require('../../lib/middleware/receive-message/conversation-set-campaign');
+const sendAskContinueMiddleware = require('../../lib/middleware/receive-message/send-ask-continue');;
 const sendGambitCampaignsReplyMiddleware = require('../../lib/middleware/receive-message/send-gambit-campaigns-reply');
-const sendCampaignMessageMiddleware = require('../../lib/middleware/receive-message/send-campaign-message');
-const outboundMessageMiddleware = require('../../lib/middleware/receive-message/message-outbound');
 
 router.use(paramsMiddleware());
 
@@ -59,17 +56,8 @@ router.use(parseAskContinueMiddleware());
 // If our last outbound template was non-Gambit, prompt to continue back with the Campaign.
 router.use(sendAskContinueMiddleware());
 
-// Check if we need to update the Campaign.
-router.use(setCampaignMiddleware());
-
-// If this message is for a Campaign Signup, post to Gambit Campaigns and send the reply we get.
+// If we haven't matched anything yet, send this message to Gambit Campaigns for a reply.
 router.use(sendGambitCampaignsReplyMiddleware());
 
-// Includes catchall for when no Campaign has been set, and no Rivescript reply matched.
-router.use(sendCampaignMessageMiddleware());
-
-// Create our outbound message and send the reply back to sender (if conversation isn't paused).
-router.use(outboundMessageMiddleware());
-router.post('/', (req, res) => helpers.sendResponse(req, res));
 
 module.exports = router;
