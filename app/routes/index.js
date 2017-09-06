9331eb16646cd83d02c69999f259090e0e345f8e
Rafa <rapala61@gmail.com>
2017-09-06 11:13:04 -0500

    added parseMetadataMiddleware to routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,18 +6,23 @@ const importMessageRoute = require('./import-message');
 
 // middleware
 const authenticateMiddleware = require('../../lib/middleware/authenticate');
+const parseMetadataMiddleware = require('../../lib/middleware/metadata-parse');
 
 module.exports = function init(app) {
   app.get('/', (req, res) => {
     res.send('hi');
   });
+
+  // authenticate all requests after this line
+  app.use(authenticateMiddleware());
+
+  // parse metadata like requestId and retryCount for all requests after this line
+  app.use(parseMetadataMiddleware());
+
   app.use('/api/v1/receive-message',
-    authenticateMiddleware(),
     receiveMessageRoute);
   app.use('/api/v1/send-message',
-    authenticateMiddleware(),
     sendMessageRoute);
   app.use('/api/v1/import-message',
-    authenticateMiddleware(),
     importMessageRoute);
 };
