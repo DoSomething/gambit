32cc0b7e15c7268f6ddd67e4e51e46489b09590b
Aaron Schachter <aaronschachter@gmail.com>
2017-10-12 20:46:26 -0700

    DRY

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,14 +14,11 @@ module.exports = function init(app) {
   app.get('/', (req, res) => res.send('hi'));
   app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
+  // TODO: Eventually remove this.
+  // @see https://github.com/DoSomething/gambit-conversations/issues/55
   app.use((req, res, next) => {
-    // TODO: DRY this with the mongoose routes.
-    res.header('Access-Control-Expose-Headers', 'X-Gambit-Results-Count');
-    // TODO: Eventually remove this.
-    // @see https://github.com/DoSomething/gambit-conversations/issues/55
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
     return next();
   });
   // restified routes
--- app/routes/mongoose.js
+++ app/routes/mongoose.js
@@ -7,6 +7,12 @@ const Message = require('../models/Message');
 const Campaign = require('../models/Campaign');
 
 const router = express.Router();
+const countHeader = 'X-Gambit-Results-Count';
+
+router.use((req, res, next) => {
+  res.header('Access-Control-Expose-Headers', countHeader);
+  return next();
+});
 
 const checkReqMethod = function checkReqMethod(req, res, next) {
   if (req.method !== 'GET') {
@@ -15,26 +21,17 @@ const checkReqMethod = function checkReqMethod(req, res, next) {
   return next();
 };
 
-const resultsHeader = 'X-Gambit-Results-Count';
-const resultsLimit = 50;
+function getRestifyOptionsWithName(name) {
+  return {
+    limit: 50,
+    name,
+    totalCountHeader: countHeader,
+    preMiddleware: checkReqMethod,
+  };
+}
 
-restify.serve(router, Conversation, {
-  limit: resultsLimit,
-  name: 'conversations',
-  totalCountHeader: resultsHeader,
-  preMiddleware: checkReqMethod,
-});
-restify.serve(router, Message, {
-  limit: resultsLimit,
-  name: 'messages',
-  totalCountHeader: resultsHeader,
-  preMiddleware: checkReqMethod,
-});
-restify.serve(router, Campaign, {
-  limit: resultsLimit,
-  name: 'campaigns',
-  totalCountHeader: resultsHeader,
-  preMiddleware: checkReqMethod,
-});
+restify.serve(router, Conversation, getRestifyOptionsWithName('conversations'));
+restify.serve(router, Message, getRestifyOptionsWithName('messages'));
+restify.serve(router, Campaign, getRestifyOptionsWithName('campaigns'));
 
 module.exports = router;
