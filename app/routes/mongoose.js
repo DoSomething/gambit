ddaea42d1e8daf364cdfa549a4ca357eead91f6b
Aaron Schachter <aaronschachter@gmail.com>
2017-10-12 20:38:22 -0700

    Adds preMiddleware

--- app/routes/mongoose.js
+++ app/routes/mongoose.js
@@ -8,22 +8,33 @@ const Campaign = require('../models/Campaign');
 
 const router = express.Router();
 
+const checkReqMethod = function checkReqMethod(req, res, next) {
+  if (req.method !== 'GET') {
+    return res.sendStatus(401);
+  }
+  return next();
+};
+
 const resultsHeader = 'X-Gambit-Results-Count';
 const resultsLimit = 50;
+
 restify.serve(router, Conversation, {
   limit: resultsLimit,
   name: 'conversations',
   totalCountHeader: resultsHeader,
+  preMiddleware: checkReqMethod,
 });
 restify.serve(router, Message, {
   limit: resultsLimit,
   name: 'messages',
   totalCountHeader: resultsHeader,
+  preMiddleware: checkReqMethod,
 });
 restify.serve(router, Campaign, {
   limit: resultsLimit,
   name: 'campaigns',
   totalCountHeader: resultsHeader,
+  preMiddleware: checkReqMethod,
 });
 
 module.exports = router;
