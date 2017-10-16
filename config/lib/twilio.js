f4c3ba263820efe27351730555b8c290aa89b55d
Rafa <rapala61@gmail.com>
2017-10-16 17:56:48 -0500

    Add test creds support for our Twilio library

--- config/lib/twilio.js
+++ config/lib/twilio.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const useTwilioTestCreds = process.env.DS_GAMBIT_CONVERSATIONS_SHOULD_USE_TWILIO_TEST_CREDS || 'false';
+const accountSid = useTwilioTestCreds === 'true' ?
+  process.env.TWILIO_TEST_ACCOUNT_SID :
+  process.env.TWILIO_ACCOUNT_SID;
+const authToken = useTwilioTestCreds === 'true' ?
+  process.env.TWILIO_TEST_AUTH_TOKEN :
+  process.env.TWILIO_AUTH_TOKEN;
+const fromNumber = useTwilioTestCreds === 'true' ?
+  process.env.TWILIO_TEST_FROM_NUMBER :
+  process.env.TWILIO_FROM_NUMBER;
+
 module.exports = {
-  accountSid: process.env.TWILIO_ACCOUNT_SID,
-  authToken: process.env.TWILIO_AUTH_TOKEN,
-  fromNumber: process.env.TWILIO_FROM_NUMBER,
+  accountSid,
+  authToken,
+  fromNumber,
   messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+  useTwilioTestCreds,
 };
