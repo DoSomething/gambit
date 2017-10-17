a7a9f2fd4219300b2f574dbf9ed6e5008376e2f4
Rafa <rapala61@gmail.com>
2017-10-17 10:13:38 -0500

    implemented feedback on naming and from number

--- config/lib/twilio.js
+++ config/lib/twilio.js
@@ -1,15 +1,13 @@
 'use strict';
 
-const useTwilioTestCreds = process.env.DS_GAMBIT_CONVERSATIONS_SHOULD_USE_TWILIO_TEST_CREDS || 'false';
+const useTwilioTestCreds = process.env.DS_GAMBIT_CONVERSATIONS_USE_TWILIO_TEST_CREDS || 'false';
 const accountSid = useTwilioTestCreds === 'true' ?
   process.env.TWILIO_TEST_ACCOUNT_SID :
   process.env.TWILIO_ACCOUNT_SID;
 const authToken = useTwilioTestCreds === 'true' ?
   process.env.TWILIO_TEST_AUTH_TOKEN :
   process.env.TWILIO_AUTH_TOKEN;
-const fromNumber = useTwilioTestCreds === 'true' ?
-  process.env.TWILIO_TEST_FROM_NUMBER :
-  process.env.TWILIO_FROM_NUMBER;
+const fromNumber = process.env.TWILIO_TEST_FROM_NUMBER || '+15005550006';
 
 module.exports = {
   accountSid,
