ee5dda81626d10fa1febf9c6f41a136f435a4e0e
Aaron Schachter <aaronschachter@gmail.com>
2017-11-01 09:01:22 -0700

    Set sms_status instead of email

--- config/lib/northstar.js
+++ config/lib/northstar.js
@@ -6,7 +6,7 @@ module.exports = {
     baseUri: process.env.DS_NORTHSTAR_API_BASEURI,
   },
   createUserOptions: {
-    defaultEmailDomain: process.env.DS_GAMBIT_CREATE_USER_DEFAULT_EMAIL_DOMAIN || 'mobile.import',
+    smsStatus: 'active',
     passwordAlgorithm: 'sha1',
     passwordKey: process.env.DS_GAMBIT_CREATE_USER_PASSWORD_KEY || 'puppetSlothForever',
     source: process.env.DS_GAMBIT_CREATE_USER_SOURCE || 'sms',
