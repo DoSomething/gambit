af2a6b35c38aac3d0bc1e3e9398b7af1181f473f
Aaron Schachter <aaronschachter@gmail.com>
2017-11-30 15:03:56 -0800

    Test helpers.sendResponseWithStatusCode

--- test/lib/helpers.test.js
+++ test/lib/helpers.test.js
@@ -16,10 +16,12 @@ chai.use(sinonChai);
 
 const sandbox = sinon.sandbox.create();
 
+// App modules
+const analyticsHelper = require('../../lib/helpers').analytics;
+const logger = require('../../lib/logger');
+
 // Module to test
 const helpers = require('../../lib/helpers');
-
-const analyticsHelper = helpers.analytics;
 
 test.afterEach(() => {
   // reset stubs, spies, and mocks
@@ -70,12 +72,45 @@ test('helpers.sendErrorResponse(res): not sending an error should use a Generic 
 });
 
 // sendResponseWithStatusCode
-test('helpers.sendResponseWithStatusCode(res, code, msg): should call analyticsHelper.addParameters', () => {
+test('helpers.sendResponseWithStatusCode(res, code, msg): should call res.status and res.send', () => {
   const res = httpMocks.createResponse();
+  sandbox.spy(res, 'status');
+  sandbox.spy(res, 'send');
   sandbox.stub(analyticsHelper, 'addParameters').returns(true);
+  const statusCode = 501;
   const message = 'Epic fail :-(';
 
-  helpers.sendErrorResponse(res, 500, message);
-
+  helpers.sendResponseWithStatusCode(res, statusCode, message);
+  res.status.should.have.been.called;
+  res.send.should.have.been.called;
+  res.statusCode.should.be.equal(statusCode);
+  const body = res._getData(); // eslint-disable-line no-underscore-dangle
+  body.message.should.be.equal(message);
   analyticsHelper.addParameters.should.have.been.called;
+});
+
+test('helpers.sendResponseWithStatusCode(res, code, msg): should call logger.error for errors', () => {
+  const res = httpMocks.createResponse();
+  sandbox.stub(logger, 'debug').returns(true);
+  sandbox.stub(logger, 'error').returns(true);
+  sandbox.stub(analyticsHelper, 'addParameters').returns(true);
+  const statusCode = 501;
+  const message = 'Oh noes another epic fail D:';
+
+  helpers.sendResponseWithStatusCode(res, statusCode, message);
+  logger.debug.should.not.have.been.called;
+  logger.error.should.have.been.called;
+});
+
+test('helpers.sendResponseWithStatusCode(res, code, msg): should call logger.debug if success', () => {
+  const res = httpMocks.createResponse();
+  sandbox.stub(logger, 'debug').returns(true);
+  sandbox.stub(logger, 'error').returns(true);
+  sandbox.stub(analyticsHelper, 'addParameters').returns(true);
+  const statusCode = 201;
+  const message = 'Great job';
+
+  helpers.sendResponseWithStatusCode(res, statusCode, message);
+  logger.error.should.not.have.been.called;
+  logger.debug.should.have.been.called;
 });
