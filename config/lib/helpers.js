7140cd55b0c89ba9645b779cb05dac510786af1c
Aaron Schachter <aaronschachter@gmail.com>
2017-12-01 16:12:15 -0800

    Adds lib/helpers/macro

--- config/lib/helpers.js
+++ config/lib/helpers.js
@@ -4,16 +4,6 @@ const menuCommand = 'menu';
 
 module.exports = {
   menuCommand,
-  macros: {
-    confirmedCampaign: 'confirmedCampaign',
-    declinedCampaign: 'declinedCampaign',
-    gambit: 'gambit',
-    sendCrisisMessage: 'sendCrisisMessage',
-    sendInfoMessage: 'sendInfoMessage',
-    subscriptionStatusLess: 'subscriptionStatusLess',
-    subscriptionStatusStop: 'subscriptionStatusStop',
-    supportRequested: 'supportRequested',
-  },
   subscriptionStatusValues: {
     active: 'active',
     less: 'less',
