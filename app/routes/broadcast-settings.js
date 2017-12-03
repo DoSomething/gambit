9e27bf45ce376094134689033c4c6849c85265da
Aaron Schachter <aaronschachter@gmail.com>
2017-12-03 14:47:00 -0800

    Adds totals middleware

--- app/routes/broadcast-settings.js
+++ app/routes/broadcast-settings.js
@@ -6,12 +6,14 @@ const router = express.Router();
 
 // Middleware
 const paramsMiddleware = require('../../lib/middleware/broadcast-settings/params');
-const broadcastMiddleware = require('../../lib/middleware/broadcast-settings/broadcast');
+const getBroadcastMiddleware = require('../../lib/middleware/broadcast-settings/broadcast');
+const totalsMiddleware = require('../../lib/middleware/broadcast-settings/totals');
 const generateSettingsMiddleware = require('../../lib/middleware/broadcast-settings/settings');
 
 router.get('/:broadcastId',
   paramsMiddleware(),
-  broadcastMiddleware(),
+  getBroadcastMiddleware(),
+  totalsMiddleware(),
   generateSettingsMiddleware());
 
 module.exports = router;
