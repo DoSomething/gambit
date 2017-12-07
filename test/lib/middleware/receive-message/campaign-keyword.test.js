3c436c082b936da551adc3cf60fc9e8422abf648
Aaron Schachter <aaronschachter@gmail.com>
2017-12-06 21:51:03 -0800

    Adds tests for continue, closed Campaign replies

--- test/lib/middleware/receive-message/campaign-keyword.test.js
+++ test/lib/middleware/receive-message/campaign-keyword.test.js
@@ -7,11 +7,18 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const httpMocks = require('node-mocks-http');
+const underscore = require('underscore');
 
 const helpers = require('../../../../lib/helpers');
 const gambitCampaigns = require('../../../../lib/gambit-campaigns');
+const stubs = require('../../../helpers/stubs');
+const conversationFactory = require('../../../helpers/factories/conversation');
 
 const requestHelper = helpers.request;
+const replies = helpers.replies;
+const mockConversation = conversationFactory.getValidConversation();
+const mockCampaign = { id: stubs.getCampaignId() };
+const mockKeyword = 'winter';
 
 chai.should();
 chai.use(sinonChai);
@@ -23,8 +30,14 @@ const getCampaignByKeyword = require('../../../../lib/middleware/receive-message
 const sandbox = sinon.sandbox.create();
 
 test.beforeEach((t) => {
-  // setup req, res mocks
+  sandbox.stub(replies, 'campaignClosed')
+    .returns(underscore.noop);
+  sandbox.stub(replies, 'continueCampaign')
+    .returns(underscore.noop);
   t.context.req = httpMocks.createRequest();
+  t.context.req.conversation = mockConversation;
+  sandbox.stub(mockConversation, 'setCampaign')
+    .returns(Promise.resolve(true));
   t.context.req.inboundMessageText = 'winter';
   t.context.res = httpMocks.createResponse();
 });
@@ -47,17 +60,70 @@ test('getCampaignByKeyword should not call gambitCampaigns.getCampaignByKeyword 
   await middleware(t.context.req, t.context.res, next);
   next.should.have.been.called;
   gambitCampaigns.getCampaignByKeyword.should.not.have.been.called;
+  t.falsy(t.context.req.campaign);
+  t.falsy(t.context.req.keyword);
 });
 
-test('getCampaignByKeyword should call gambitCampaigns.getCampaignByKeyword if requestHelper.parseCampaignKeyword', async (t) => {
+test('getCampaignByKeyword should call next if gambitCampaigns.getCampaignByKeyword returns null', async (t) => {
   // setup
   const next = sinon.stub();
   const middleware = getCampaignByKeyword();
   sandbox.stub(requestHelper, 'parseCampaignKeyword')
-    .returns('winter');
-  sandbox.stub(gambitCampaigns, 'getCampaignByKeyword').returns(Promise.resolve(true));
+    .returns(mockKeyword);
+  sandbox.stub(gambitCampaigns, 'getCampaignByKeyword')
+    .returns(Promise.resolve(null));
 
   // test
   await middleware(t.context.req, t.context.res, next);
   gambitCampaigns.getCampaignByKeyword.should.have.been.called;
+  t.context.req.conversation.setCampaign.should.not.have.been.called;
+  next.should.have.been.called;
+  t.falsy(t.context.req.campaign);
+  t.falsy(t.context.req.keyword);
+});
+
+test('getCampaignByKeyword should call replies.campaignClosed if keyword Campaign is closed', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = getCampaignByKeyword();
+
+  sandbox.stub(requestHelper, 'parseCampaignKeyword')
+    .returns(mockKeyword);
+  sandbox.stub(gambitCampaigns, 'getCampaignByKeyword')
+    .returns(Promise.resolve(mockCampaign));
+  sandbox.stub(gambitCampaigns, 'isClosedCampaign')
+    .returns(true);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  gambitCampaigns.getCampaignByKeyword.should.have.been.called;
+  t.context.req.conversation.setCampaign.should.have.been.called;
+  gambitCampaigns.isClosedCampaign.should.have.been.called;
+  t.context.req.campaign.should.equal(mockCampaign);
+  t.context.req.keyword.should.equal(mockKeyword);
+  replies.continueCampaign.should.not.have.been.called;
+  replies.campaignClosed.should.have.been.called;
+});
+
+test('getCampaignByKeyword should call replies.continueCampaign if keyword Campaign is active', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = getCampaignByKeyword();
+
+  sandbox.stub(requestHelper, 'parseCampaignKeyword')
+    .returns(mockKeyword);
+  sandbox.stub(gambitCampaigns, 'getCampaignByKeyword')
+    .returns(Promise.resolve(mockCampaign));
+  sandbox.stub(gambitCampaigns, 'isClosedCampaign')
+    .returns(false);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  gambitCampaigns.getCampaignByKeyword.should.have.been.called;
+  t.context.req.conversation.setCampaign.should.have.been.called;
+  gambitCampaigns.isClosedCampaign.should.have.been.called;
+  t.context.req.campaign.should.equal(mockCampaign);
+  t.context.req.keyword.should.equal(mockKeyword);
+  replies.continueCampaign.should.have.been.called;
+  replies.campaignClosed.should.not.have.been.called;
 });
