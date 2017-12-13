81836037be104050cadbeadff8037e4b60b25642
Aaron Schachter <aaronschachter@gmail.com>
2017-12-13 12:43:41 -0800

    Adds config.customUrl.queryValue property

--- config/lib/helpers/tags.js
+++ config/lib/helpers/tags.js
@@ -4,6 +4,14 @@ module.exports = {
   customUrl: {
     url: process.env.DS_GAMBIT_CUSTOM_URL || 'https://dosomething.turbovote.org',
     queryParamName: process.env.DS_GAMBIT_CUSTOM_URL_QUERY_PARAM || 'referral-code',
+    queryValue: {
+      separator: ',',
+      fieldSuffix: ':',
+      fields: {
+        userId: 'user',
+        campaignRunId: 'campaign',
+      },
+    },
   },
   tags: {
     customUrl: 'custom_url',
