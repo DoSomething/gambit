a76b7d77a5059ea18e760a835df9b2b6a0c3a103
Aaron Schachter <aaronschachter@gmail.com>
2017-12-13 22:55:03 -0800

    Moves Broadcast cache to cache helper

--- config/lib/gambit-campaigns.js
+++ config/lib/gambit-campaigns.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   activeCampaignsCacheKey: 'active',
-  cacheTtl: process.env.DS_GAMBIT_CONVERSATIONS_CAMPAIGNS_CACHE_TTL || 300,
   clientOptions: {
     baseUri: process.env.DS_GAMBIT_CAMPAIGNS_API_BASEURI,
     apiKey: process.env.DS_GAMBIT_CAMPAIGNS_API_KEY,
--- config/lib/helpers/cache.js
+++ config/lib/helpers/cache.js
@@ -6,6 +6,10 @@ function ttl(value) {
 }
 
 module.exports = {
+  broadcasts: {
+    name: 'broadcasts',
+    ttl: ttl(process.env.DS_GAMBIT_CONVERSATIONS_BROADCASTS_CACHE_TTL),
+  },
   broadcastStats: {
     name: 'broadcastStats',
     ttl: ttl(process.env.DS_GAMBIT_CONVERSATIONS_BROADCAST_STATS_CACHE_TTL),
