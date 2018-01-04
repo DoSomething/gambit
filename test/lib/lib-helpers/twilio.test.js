fb9a1c4b6f14eb5cd231db561f96da0d0690bd25
Aaron Schachter <aaronschachter@gmail.com>
2018-01-03 22:33:39 -0800

    Starts on twilio helper test

---
+++ test/lib/lib-helpers/twilio.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('dotenv').config();
+const test = require('ava');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const httpMocks = require('node-mocks-http');
+
+const stubs = require('../../helpers/stubs');
+
+chai.should();
+chai.use(sinonChai);
+
+// module to be tested
+const twilioHelper = require('../../../lib/helpers/twilio');
+
+// sinon sandbox object
+const sandbox = sinon.sandbox.create();
+
+const mockTwilioRequestBody = stubs.getMockInboundTwilioRequestBody();
+
+test.beforeEach((t) => {
+  t.context.req = httpMocks.createRequest();
+  t.context.req.body = mockTwilioRequestBody;
+});
+
+test.afterEach(() => {
+  sandbox.restore();
+});
+
+// parseBody
+test('parseBody should inject vars into req', (t) => {
+  sandbox.spy(twilioHelper, 'parseUserLocationFromReq');
+  twilioHelper.parseBody(t.context.req);
+  twilioHelper.parseUserLocationFromReq.should.have.been.called;
+  t.context.req.platformUserId.should.equal(mockTwilioRequestBody.From);
+  t.context.req.should.have.property('platformUserLocation');
+});
+
+// hasLocation
+test('parseUserLocationFromReq should return an object', (t) => {
+  const result = twilioHelper.parseUserLocationFromReq(t.context.req);
+  result.addr_city.should.equal(mockTwilioRequestBody.FromCity);
+  result.addr_state.should.equal(mockTwilioRequestBody.FromState);
+  result.addr_zip.should.equal(mockTwilioRequestBody.FromZip);
+  result.country.should.equal(mockTwilioRequestBody.FromCountry);
+});
--- test/lib/lib-helpers/user.test.js
+++ test/lib/lib-helpers/user.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// libs
 require('dotenv').config();
 const test = require('ava');
 const chai = require('chai');
@@ -8,20 +7,16 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const userFactory = require('../../helpers/factories/user');
 
-// setup "x.should.y" assertion style
 chai.should();
 chai.use(sinonChai);
-
-// const config = require('../../../config/lib/helpers/tags');
 
 // module to be tested
 const userHelper = require('../../../lib/helpers/user');
 
 // sinon sandbox object
 const sandbox = sinon.sandbox.create();
-// Cleanup
+
 test.afterEach(() => {
-  // reset stubs, spies, and mocks
   sandbox.restore();
 });
 
