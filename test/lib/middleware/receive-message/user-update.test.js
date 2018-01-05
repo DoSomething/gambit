e531384b46a0fb53c91779cdffb50d00def436cf
Aaron Schachter <aaronschachter@gmail.com>
2018-01-05 13:36:58 -0800

    Adds tests for stop and less

--- test/lib/middleware/receive-message/user-update.test.js
+++ test/lib/middleware/receive-message/user-update.test.js
@@ -12,8 +12,11 @@ const underscore = require('underscore');
 
 const northstar = require('../../../../lib/northstar');
 const helpers = require('../../../../lib/helpers');
+const stubs = require('../../../helpers/stubs');
 const userFactory = require('../../../helpers/factories/user');
 
+const repliesHelper = helpers.replies;
+const subscriptionHelper = helpers.subscription;
 const userHelper = helpers.user;
 
 // setup "x.should.y" assertion style
@@ -34,8 +37,13 @@ const userUpdateFailStub = () => Promise.reject({ message: 'Epic fail' });
 test.beforeEach((t) => {
   sandbox.stub(helpers, 'sendErrorResponse')
     .returns(underscore.noop);
+  sandbox.stub(repliesHelper, 'subscriptionStatusLess')
+    .returns(underscore.noop);
+  sandbox.stub(repliesHelper, 'subscriptionStatusStop')
+    .returns(underscore.noop);
   t.context.req = httpMocks.createRequest();
   t.context.req.user = mockUser;
+  t.context.req.rivescriptReplyText = stubs.getRandomMessageText();
   t.context.res = httpMocks.createResponse();
 });
 
@@ -50,14 +58,20 @@ test('updateUser should call next if Northstar.updateUser success', async (t) =>
   const middleware = updateUser();
   sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
     .returns({ });
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(null);
   sandbox.stub(northstar, 'updateUser')
     .callsFake(userUpdateStub);
 
   // test
   await middleware(t.context.req, t.context.res, next);
   userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
   northstar.updateUser.should.have.been.called;
+  repliesHelper.subscriptionStatusLess.should.not.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.not.have.been.called;
   next.should.have.been.called;
+  helpers.sendErrorResponse.should.not.have.been.called;
 });
 
 test('updateUser should call sendErrorResponse if Northstar.updateUser fails', async (t) => {
@@ -66,13 +80,63 @@ test('updateUser should call sendErrorResponse if Northstar.updateUser fails', a
   const middleware = updateUser();
   sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
     .returns({ });
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(null);
   sandbox.stub(northstar, 'updateUser')
     .callsFake(userUpdateFailStub);
 
   // test
   await middleware(t.context.req, t.context.res, next);
   userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
   northstar.updateUser.should.have.been.called;
+  repliesHelper.subscriptionStatusLess.should.not.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.not.have.been.called;
   next.should.not.have.been.called;
   helpers.sendErrorResponse.should.have.been.called;
 });
+
+test('updateUser should call replies.subscriptionStatusLess if statusUpdate is less', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = updateUser();
+  sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
+    .returns({ });
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(subscriptionHelper.statuses.less());
+  sandbox.stub(northstar, 'updateUser')
+    .callsFake(userUpdateStub);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
+  northstar.updateUser.should.have.been.called;
+  repliesHelper.subscriptionStatusLess.should.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.not.have.been.called;
+  next.should.not.have.been.called;
+  helpers.sendErrorResponse.should.not.have.been.called;
+});
+
+test('updateUser should call replies.subscriptionStatusStop if statusUpdate is stop', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = updateUser();
+  sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
+    .returns({ });
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(subscriptionHelper.statuses.stop());
+  sandbox.stub(northstar, 'updateUser')
+    .callsFake(userUpdateStub);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
+  northstar.updateUser.should.have.been.called;
+  repliesHelper.subscriptionStatusLess.should.not.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.have.been.called;
+  next.should.not.have.been.called;
+  helpers.sendErrorResponse.should.not.have.been.called;
+});
+
