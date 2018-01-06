0d82468fd29dd96edfbe2014de4a7fa299d53a43
Aaron Schachter <aaronschachter@gmail.com>
2018-01-06 08:01:25 -0800

    Adds hasAddress tests

--- test/lib/middleware/receive-message/user-update.test.js
+++ test/lib/middleware/receive-message/user-update.test.js
@@ -31,6 +31,10 @@ const sandbox = sinon.sandbox.create();
 
 // stubs
 const mockUser = userFactory.getValidUser();
+const mockDefaultUserUpdateData = {
+  last_messaged_at: Date.now(),
+  sms_paused: false,
+};
 const userUpdateStub = () => Promise.resolve(mockUser);
 const userUpdateFailStub = () => Promise.reject({ message: 'Epic fail' });
 
@@ -57,7 +61,7 @@ test('updateUser should call next if Northstar.updateUser success', async (t) =>
   const next = sinon.stub();
   const middleware = updateUser();
   sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
-    .returns({ });
+    .returns(mockDefaultUserUpdateData);
   sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
     .returns(null);
   sandbox.stub(northstar, 'updateUser')
@@ -67,6 +71,7 @@ test('updateUser should call next if Northstar.updateUser success', async (t) =>
   await middleware(t.context.req, t.context.res, next);
   userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
   userHelper.getSubscriptionStatusUpdate.should.have.been.called;
+  t.context.req.userUpdateData.should.deep.equal(mockDefaultUserUpdateData);
   northstar.updateUser.should.have.been.called;
   repliesHelper.subscriptionStatusLess.should.not.have.been.called;
   repliesHelper.subscriptionStatusStop.should.not.have.been.called;
@@ -79,7 +84,7 @@ test('updateUser should call sendErrorResponse if Northstar.updateUser fails', a
   const next = sinon.stub();
   const middleware = updateUser();
   sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
-    .returns({ });
+    .returns(mockDefaultUserUpdateData);
   sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
     .returns(null);
   sandbox.stub(northstar, 'updateUser')
@@ -140,3 +145,57 @@ test('updateUser should call replies.subscriptionStatusStop if statusUpdate is s
   helpers.sendErrorResponse.should.not.have.been.called;
 });
 
+test('updateUser should not set address if req.user.platformUserAddress undefined', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = updateUser();
+  sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
+    .returns(mockDefaultUserUpdateData);
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(null);
+  sandbox.stub(userHelper, 'hasAddress')
+    .returns(false);
+  sandbox.stub(northstar, 'updateUser')
+    .callsFake(userUpdateStub);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
+  userHelper.hasAddress.should.not.have.been.called;
+  t.context.req.userUpdateData.should.not.have.property('country');
+  northstar.updateUser.should.have.been.called;
+  repliesHelper.subscriptionStatusLess.should.not.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.not.have.been.called;
+  next.should.have.been.called;
+  helpers.sendErrorResponse.should.not.have.been.called;
+});
+
+test('updateUser should set address when req.user does not have address', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = updateUser();
+  sandbox.stub(userHelper, 'getDefaultUpdatePayloadFromReq')
+    .returns(mockDefaultUserUpdateData);
+  sandbox.stub(userHelper, 'getSubscriptionStatusUpdate')
+    .returns(null);
+  sandbox.stub(userHelper, 'hasAddress')
+    .returns(false);
+  sandbox.stub(northstar, 'updateUser')
+    .callsFake(userUpdateStub);
+  t.context.req.userId = mockUser.id;
+  t.context.req.platformUserAddress = { country: 'US' };
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  userHelper.getDefaultUpdatePayloadFromReq.should.have.been.called;
+  userHelper.getSubscriptionStatusUpdate.should.have.been.called;
+  userHelper.hasAddress.should.have.been.called;
+  t.context.req.userUpdateData.should.have.property('country');
+  northstar.updateUser
+    .should.have.been.calledWith(t.context.req.userId, t.context.req.userUpdateData);
+  repliesHelper.subscriptionStatusLess.should.not.have.been.called;
+  repliesHelper.subscriptionStatusStop.should.not.have.been.called;
+  next.should.have.been.called;
+  helpers.sendErrorResponse.should.not.have.been.called;
+});
