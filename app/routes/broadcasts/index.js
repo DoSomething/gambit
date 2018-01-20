4577038bf6dd8c32fe46a159b7eb0d04a226a366
Aaron Schachter <aaronschachter@gmail.com>
2018-01-19 22:47:29 -0800

    Adds lib/middleware/broadcasts dir

--- app/routes/broadcasts/index.js
+++ app/routes/broadcasts/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // Middleware
-const indexMiddleware = require('../../../lib/middleware/broadcasts-index');
+const indexMiddleware = require('../../../lib/middleware/broadcasts/index');
 
 router.get('/', indexMiddleware());
 
