30da9ea1448eb1cf132b507b2ce31bdf45e8254d
Aaron Schachter <aaronschachter@gmail.com>
2018-02-06 15:22:08 -0800

    Renames support/conversation-update

--- app/routes/messages/front.js
+++ app/routes/messages/front.js
@@ -9,23 +9,18 @@ const outboundMessageConfig = require('../../../config/lib/middleware/messages/m
 
 // Middleware
 const paramsMiddleware = require('../../../lib/middleware/messages/support/params');
-// const getUserMiddleware = require('../../lib/middleware/messages/user-get');
-// We'll need to tweak validation to allow sending message paused Users.
-// const validateUserMiddleware = require('../../lib/middleware/messages/user-validate');
 const getConversationMiddleware = require('../../../lib/middleware/conversation-get');
-const frontMiddleware = require('../../../lib/middleware/messages/support/front');
+const updateConversationMiddleware = require('../../../lib/middleware/messages/support/conversation-update');
 // Note: we're not adding the Load Outbound Message middleware, because Front messages are posted
 // directly to Gambit Conversations, which means we're not retrying any failed requests here.
 const createOutboundMessageMiddleware = require('../../../lib/middleware/message-outbound-create');
 
 router.use(paramsMiddleware());
-
-// Fetch Northstar User.
-// router.use(getUserMiddleware());
-// router.use(validateUserMiddleware());
-
 router.use(getConversationMiddleware());
-router.use(frontMiddleware());
+
+// TODO: Fetch the User for the Conversation, verify they are not unsubscribed.
+
+router.use(updateConversationMiddleware());
 router.use(createOutboundMessageMiddleware(outboundMessageConfig));
 
 module.exports = router;
