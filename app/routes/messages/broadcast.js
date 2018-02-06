9c6c33ef5cd4ca70bd0fa3943bab3b2e833c57f7
Aaron Schachter <aaronschachter@gmail.com>
2018-02-06 14:20:09 -0800

    Refactors as messages/message-outbound config

--- app/routes/messages/broadcast.js
+++ app/routes/messages/broadcast.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // Middleware configs
-const outboundMessageConfig = require('../../../config/lib/middleware/messages/broadcast/message-outbound');
+const outboundMessageConfig = require('../../../config/lib/middleware/messages/message-outbound');
 
 // Middleware
 const paramsMiddleware = require('../../../lib/middleware/messages/broadcast/params');
--- app/routes/messages/front.js
+++ app/routes/messages/front.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // Middleware configs
-const outboundMessageConfig = require('../../../config/lib/middleware/messages/support/message-outbound');
+const outboundMessageConfig = require('../../../config/lib/middleware/messages/message-outbound');
 
 // Middleware
 const paramsMiddleware = require('../../../lib/middleware/messages/support/params');
