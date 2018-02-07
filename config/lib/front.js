81370c276a407d7c499127d027de0d2f04ac643b
Aaron Schachter <aaronschachter@gmail.com>
2018-02-06 21:51:07 -0800

    Moves validation into Front lib

--- config/lib/front.js
+++ config/lib/front.js
@@ -5,6 +5,10 @@ module.exports = {
     baseUri: process.env.FRONT_API_BASEURI || 'https://api2.frontapp.com',
     apiSecret: process.env.FRONT_API_SECRET,
     apiToken: process.env.FRONT_API_TOKEN,
+    signatureAlgorithm: 'sha1',
+    signatureEncoding: 'base64',
+    signatureHeader: 'x-front-signature',
+    validationDisabled: process.env.FRONT_VALIDATION_DISABLED || false,
   },
   channels: {
     support: process.env.FRONT_API_SUPPORT_CHANNEL,
