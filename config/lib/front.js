b6189b5128f9e795ab2138ded15344d96710134c
Aaron Schachter <aaronschachter@gmail.com>
2018-02-06 22:45:50 -0800

    Adds config.crypto

--- config/lib/front.js
+++ config/lib/front.js
@@ -5,12 +5,14 @@ module.exports = {
     baseUri: process.env.FRONT_API_BASEURI || 'https://api2.frontapp.com',
     apiSecret: process.env.FRONT_API_SECRET,
     apiToken: process.env.FRONT_API_TOKEN,
-    signatureAlgorithm: 'sha1',
-    signatureEncoding: 'base64',
     signatureHeader: 'x-front-signature',
     validationDisabled: process.env.FRONT_VALIDATION_DISABLED || false,
   },
   channels: {
     support: process.env.FRONT_API_SUPPORT_CHANNEL,
+  },
+  crypto: {
+    algorithm: 'sha1',
+    encoding: 'base64',
   },
 };
