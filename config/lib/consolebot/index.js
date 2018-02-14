34684990fb043d68b21f53e453138d1d25a96262
Aaron Schachter <aaronschachter@gmail.com>
2018-02-13 22:02:03 -0800

    Mock Twilio request for User address

--- config/lib/consolebot/index.js
+++ config/lib/consolebot/index.js
@@ -10,14 +10,21 @@ const photoUrl = 'https://www.wired.com/wp-content/uploads/2015/03/The-X-Files1-
 const basicAuthString = `${process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_NAME || 'puppet'}:${process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_PASS || 'totallysecret'}`;
 
 const configVars = {
-  apiKey: appConfig.apiKey,
   introFilePath: path.resolve(__dirname, 'intro.txt'),
-  mobile: process.env.DS_CONSOLEBOT_MOBILE,
   prompt: process.env.DS_CONSOLEBOT_PROMPT || 'You>',
   replyPrefix: process.env.DS_CONSOLEBOT_REPLY_PREFIX || 'Bot>',
   replyColor: process.env.DS_CONSOLEBOT_REPLY_COLOR || 'magenta',
-  url: `http://${basicAuthString}@localhost:${appConfig.port}/api/v2/messages?origin=twilio`,
   photoUrl: process.env.DS_CONSOLEBOT_PHOTO_URL || photoUrl,
+  request: {
+    url: `http://${basicAuthString}@localhost:${appConfig.port}/api/v2/messages?origin=twilio`,
+    body: {
+      From: process.env.DS_CONSOLEBOT_USER_MOBILE,
+      FromCity: process.env.DS_CONSOLEBOT_USER_CITY || 'Winterfell',
+      FromState: process.env.DS_CONSOLEBOT_USER_STATE || 'NJ',
+      FromZip: process.env.DS_CONSOLEBOT_USER_ZIP || '07302',
+      FromCountry: process.env.DS_CONSOLEBOT_USER_COUNTRY || 'US',
+    },
+  },
 };
 
 module.exports = configVars;
