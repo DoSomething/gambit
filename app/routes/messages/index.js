13c38b3f2ea60ac4c02a67ba9130efa7c348b2b4
Rafa <rapala61@gmail.com>
2018-03-22 16:23:42 -0500

    added status/params middleware to parse message status update properties before applying the update. Paves the way to introduce a middleware to update users when their message status update contains undeliverable errors

--- app/routes/messages/index.js
+++ app/routes/messages/index.js
@@ -5,35 +5,20 @@ const express = require('express');
 const router = express.Router();
 
 const analyticsHelper = require('../../../lib/helpers/analytics');
-const Message = require('../../models/Message');
 const broadcastMessagesRoute = require('./broadcast');
 const frontMessagesRoute = require('./front');
 const memberMessagesRoute = require('./member');
 const signupMessagesRoute = require('./signup');
-const helpers = require('../../../lib/helpers');
 
-router.patch('/:messageId', (req, res) => {
-  // TODO: Move to middleware?
-  // TODO: update user to undeliverable when `failedAt` and the following codes are detected:
-  //
-  // 30006: Landline or unreachable carrier
-  //        Example: https://www.twilio.com/console/sms/logs/SM933e25ea371f4f10a213aef5180245e1
-  // 30004: Message blocked
-  // 30008: Unknown error (?)
-  //        Example: https://www.twilio.com/console/sms/logs/SMdc6b3e74246c437ab3328f346ce86ef4
-  // 30003: Unreachable destination handset (?)
-  //        Example: https://www.twilio.com/console/sms/logs/SM4367f21c9cfb40e2b87216227a20a109
-  //
-  helpers.util.deepUpdateWithDotNotationParser(req.body)
-    .then((update) => {
-      Message.findByIdAndUpdate(req.params.messageId, update)
-        .then(() => res.sendStatus(204))
-        .catch(error => helpers.sendErrorResponse(res, error));
-    });
-  analyticsHelper.addCustomAttributes({
-    messageId: req.params.messageId,
-  });
-});
+// Middleware
+const { middleware: statusParamsMiddleware } = require('../../../lib/middleware/messages/status/params');
+const { middleware: messageUpdateMiddleware } = require('../../../lib/middleware/messages/message-update');
+
+router.patch('/:messageId',
+  statusParamsMiddleware(),
+  messageUpdateMiddleware(),
+  /* TODO: Update user if req.undeliverableError is true */
+  (req, res) => res.sendStatus(204));
 
 router.post('/', (req, res, next) => {
   const origin = req.query.origin;
---
+++ config/lib/helpers/twilio.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  // @see https://www.twilio.com/docs/api/messaging/message#delivery-related-errors
+  // 30006: Landline or unreachable carrier
+  //        Example: https://www.twilio.com/console/sms/logs/SM933e25ea371f4f10a213aef5180245e1
+  // 30004: Message blocked
+  // 30008: Unknown error
+  //        Example: https://www.twilio.com/console/sms/logs/SMdc6b3e74246c437ab3328f346ce86ef4
+  // 30003: Unreachable destination handset
+  //        Example: https://www.twilio.com/console/sms/logs/SM4367f21c9cfb40e2b87216227a20a109
+  undeliverableErrorCodes: {
+    30006: 'Landline or unreachable carrier',
+    30004: 'Message blocked',
+    // TODO: Not sure about these ones yet
+    // 30008: 'Unknown error',
+    // 30003: 'Unreachable destination handset',
+  },
+};
