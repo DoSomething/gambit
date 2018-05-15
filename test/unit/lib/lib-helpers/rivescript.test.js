cb4401764307858ad04e25a5f541a606a3d94b71
Aaron Schachter <aaronschachter@gmail.com>
2018-05-14 20:11:26 -0700

    Adds test for fetchDefaultRivescriptTopicTriggers

--- test/unit/lib/lib-helpers/rivescript.test.js
+++ test/unit/lib/lib-helpers/rivescript.test.js
@@ -6,11 +6,12 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
-chai.should();
-chai.use(sinonChai);
-
+const contentful = require('../../../../lib/contentful');
 const config = require('../../../../config/lib/helpers/rivescript');
 const stubs = require('../../../helpers/stubs');
+
+chai.should();
+chai.use(sinonChai);
 
 // module to be tested
 const rivescriptHelper = require('../../../../lib/helpers/rivescript');
@@ -24,6 +25,44 @@ const sandbox = sinon.sandbox.create();
 
 test.afterEach(() => {
   sandbox.restore();
+});
+
+// fetchDefaultRivescriptTopicTriggers
+test('fetchDefaultRivescriptTopicTriggers should call parseDefaultRivescriptTopicTrigger on contentful.fetchDefaultRivescriptTopicTriggers success', async () => {
+  // TODO: Create a defaultRivescriptTopicTrigger factory to replace these objects.
+  const firstMockEntry = { trigger: stubs.getRandomWord() };
+  const secondMockEntry = { trigger: stubs.getRandomWord() };
+  const mockEntries = [firstMockEntry, secondMockEntry];
+  sandbox.stub(contentful, 'fetchDefaultRivescriptTopicTriggers')
+    .returns(Promise.resolve(mockEntries));
+  sandbox.stub(rivescriptHelper, 'parseDefaultRivescriptTopicTrigger')
+    .returns(mockRivescriptLine);
+
+  const result = await rivescriptHelper.fetchDefaultRivescriptTopicTriggers();
+  mockEntries.forEach((entry) => {
+    rivescriptHelper.parseDefaultRivescriptTopicTrigger.should.have.been.calledWith(entry);
+  });
+  contentful.fetchDefaultRivescriptTopicTriggers.should.have.been.called;
+  result.should.deep.equal([mockRivescriptLine, mockRivescriptLine]);
+});
+
+test('fetchDefaultRivescriptTopicTriggers should return contentful.fetchDefaultRivescriptTopicTriggers error on fail', async (t) => {
+  const mockError = new Error('epic fail');
+  sandbox.stub(contentful, 'fetchDefaultRivescriptTopicTriggers')
+    .returns(Promise.reject(mockError));
+  sandbox.stub(rivescriptHelper, 'parseDefaultRivescriptTopicTrigger')
+    .returns(mockRivescriptLine);
+
+  const result = await t.throws(rivescriptHelper.fetchDefaultRivescriptTopicTriggers());
+  contentful.fetchDefaultRivescriptTopicTriggers.should.have.been.called;
+  rivescriptHelper.parseDefaultRivescriptTopicTrigger.should.not.have.been.called;
+  result.should.deep.equal(mockError);
+});
+
+// formatRivescriptLine
+test('formatRivescriptLine should return a trimmed concat of operator and value args', () => {
+  const result = rivescriptHelper.formatRivescriptLine(mockRivescriptCommandOperator, `${mockWord}   `);
+  result.should.equal(mockRivescriptLine);
 });
 
 // getRedirectCommandFromText
@@ -56,11 +95,5 @@ test('getTriggerCommandFromText should return formatRivescriptLine with trigger 
 
   const result = rivescriptHelper.getTriggerCommandFromText(mockWord);
   rivescriptHelper.formatRivescriptLine.should.have.been.calledWith(triggerCommand, mockWord);
-  result.should.equal(mockRivescriptLine);
-});
-
-// formatRivescriptLine
-test('formatRivescriptLine should return a trimmed concat of operator and value args', () => {
-  const result = rivescriptHelper.formatRivescriptLine(mockRivescriptCommandOperator, `${mockWord}   `);
   result.should.equal(mockRivescriptLine);
 });
