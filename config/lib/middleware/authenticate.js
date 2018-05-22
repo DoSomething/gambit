70ec06df29d427a7300c7dcbf937a99cd2a231bb
Rafa <rapala61@gmail.com>
2018-05-22 09:03:51 -0500

    moved some hardcoded values from the authenticate middleware to the authenticate middleware config

--- config/lib/middleware/authenticate.js
+++ config/lib/middleware/authenticate.js
@@ -5,4 +5,6 @@ module.exports = {
     name: process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_NAME,
     pass: process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_PASS,
   },
+  unauthorizedErrorMessage: 'Invalid or missing auth parameters. Unauthorized.',
+  unauthorizedErrorCode: 401,
 };
