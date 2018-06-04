552ddd2781195ad90d47d6dc5dbeff0b121fbf2d
Rafa <rapala61@gmail.com>
2018-06-04 17:41:11 -0500

    upgraded Node to 8.11.1

---
+++ config/lib/bertly.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  // TODO: Add local default
+  baseUri: process.env.DS_BERTLY_API_BASEURI,
+  apiKey: process.env.DS_BERTLY_API_KEY || 'totallysecret',
+  apiKeyHeader: process.env.DS_BERTLY_API_KEY_HEADER || 'X-BERTLY-API-KEY',
+};
