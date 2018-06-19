429b5a5734cf35baa71f5c82b0ff2cd1a44c7ae3
Aaron Schachter <aaronschachter@gmail.com>
2018-06-19 13:43:49 -0700

    Sets status as pending per https://github.com/DoSomething/northstar/pull/768

--- config/lib/helpers/subscription.js
+++ config/lib/helpers/subscription.js
@@ -4,9 +4,7 @@ module.exports = {
   subscriptionStatuses: {
     active: 'active',
     less: 'less',
-    // TODO: Set to 'pending' once available as an option.
-    // @see https://www.pivotaltracker.com/n/projects/2019429/stories/158439520
-    pending: 'unknown',
+    pending: 'pending',
     stop: 'stop',
     undeliverable: 'undeliverable',
   },
