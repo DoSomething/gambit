76946ccdac15433fbb61e0be25310f8edd8c95c6
Aaron Schachter <aaronschachter@gmail.com>
2018-06-18 23:13:00 -0700

    Updates user.sms_status to pending if topic changes to askSubscriptionStatus

--- config/lib/helpers/subscription.js
+++ config/lib/helpers/subscription.js
@@ -4,6 +4,9 @@ module.exports = {
   subscriptionStatuses: {
     active: 'active',
     less: 'less',
+    // TODO: Set to 'pending' once available as an option.
+    // @see https://www.pivotaltracker.com/n/projects/2019429/stories/158439520
+    pending: 'unknown',
     stop: 'stop',
     undeliverable: 'undeliverable',
   },
