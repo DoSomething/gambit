4555cfa7c00ed5d68a275948c9f76f625beca236
Aaron Schachter <aaronschachter@gmail.com>
2018-06-26 20:29:18 -0700

    Finishes tests for menu macro middleware

--- test/unit/lib/middleware/messages/member/macro-menu.test.js
+++ test/unit/lib/middleware/messages/member/macro-menu.test.js
@@ -21,6 +21,7 @@ const menuMacro = require('../../../../../../lib/middleware/messages/member/macr
 
 const mockConversation = conversationFactory.getValidConversation();
 const mockCampaign = campaignFactory.getValidCampaign();
+const mockError = { status: 500, message: 'Epic fail' };
 
 const sandbox = sinon.sandbox.create();
 
@@ -49,4 +50,47 @@ test('menuMacro calls next if request is not a menu macro', async (t) => {
   await middleware(t.context.req, t.context.res, next);
   helpers.campaign.fetchRandomCampaignExcludingCampaignId.should.not.have.been.called;
   next.should.have.been.called;
+});
+
+test('menuMacro calls fetchRandomCampaignExcludingCampaignId if request is a menu macro', async (t) => {
+  const next = sinon.stub();
+  const middleware = menuMacro();
+  sandbox.stub(helpers.request, 'isMenuMacro')
+    .returns(true);
+  sandbox.stub(helpers.campaign, 'fetchRandomCampaignExcludingCampaignId')
+    .returns(Promise.resolve(mockCampaign));
+  sandbox.stub(helpers.request, 'changeTopicByCampaign')
+    .returns(Promise.resolve(true));
+  sandbox.stub(helpers.replies, 'askSignup')
+    .returns(underscore.noop);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  helpers.campaign.fetchRandomCampaignExcludingCampaignId
+    .should.have.been.calledWith(t.context.req.conversation.campaignId);
+  helpers.request.changeTopicByCampaign.should.have.been.calledWith(t.context.req, mockCampaign);
+  helpers.replies.askSignup.should.have.been.calledWith(t.context.req, t.context.res);
+  next.should.not.have.been.called;
+});
+
+test('menuMacro calls sendError response if fetchRandomCampaignExcludingCampaignId fails', async (t) => {
+  const next = sinon.stub();
+  const middleware = menuMacro();
+  sandbox.stub(helpers.request, 'isMenuMacro')
+    .returns(true);
+  sandbox.stub(helpers.campaign, 'fetchRandomCampaignExcludingCampaignId')
+    .returns(Promise.reject(mockError));
+  sandbox.stub(helpers.request, 'changeTopicByCampaign')
+    .returns(Promise.resolve(true));
+  sandbox.stub(helpers.replies, 'askSignup')
+    .returns(underscore.noop);
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  helpers.campaign.fetchRandomCampaignExcludingCampaignId
+    .should.have.been.calledWith(t.context.req.conversation.campaignId);
+  helpers.request.changeTopicByCampaign.should.not.have.been.called;
+  helpers.replies.askSignup.should.not.have.been.called;
+  next.should.not.have.been.called;
+  helpers.sendErrorResponse.should.have.been.calledWith(t.context.res, mockError);
 });
