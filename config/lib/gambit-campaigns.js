5060bb5538cf07943feb181a5c2e25b3e2ffff35
Aaron Schachter <aaronschachter@gmail.com>
2018-07-16 11:01:03 -0700

    Cleanup

--- config/lib/gambit-campaigns.js
+++ config/lib/gambit-campaigns.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-  activeCampaignsCacheKey: 'active',
   clientOptions: {
     baseUri: process.env.DS_GAMBIT_CAMPAIGNS_API_BASEURI,
     apiKey: process.env.DS_GAMBIT_CAMPAIGNS_API_KEY,
--- test/helpers/factories/broadcast.js
+++ test/helpers/factories/broadcast.js
@@ -1,33 +1,39 @@
 'use strict';
 
-function getValidCampaignBroadcast() {
+const stubs = require('../stubs');
+
+/**
+ * @see https://github.com/DoSomething/gambit-campaigns/tree/master/documentation
+ */
+
+/**
+ * @return {Object}
+ */
+function getValidCampaignBroadcast(date = Date.now()) {
   return {
-    id: '38Diqre71mCa2WIsGCIyus',
-    name: 'FeedingBetterFutures2018_Jan30_Niche_TestA',
-    createdAt: '2018-01-30T16:40:03.664Z',
-    updatedAt: '2018-01-30T16:40:31.526Z',
+    id: stubs.getBroadcastId(),
+    name: stubs.getBroadcastName(),
+    createdAt: date,
+    updatedAt: date,
     message: {
-      text: 'Hi it\'s Freddie with a new scholarship opportunity! Today share a fact about food related issues online for the chance to win a $5K scholarship. You in? Y or N?',
+      text: stubs.getBroadcastMessageText(),
       attachments: [],
       template: 'askSignup',
     },
     campaignId: 7984,
+    topic: null,
   };
 }
 
-function getValidTopicBroadcast() {
-  return {
-    id: 'Z4LXfz4AEeWGMCIqAooM2',
-    name: 'GunViolence2018_March20_WeekOfAction_Day2_Attending',
-    createdAt: '2018-03-20T16:36:38.959Z',
-    updatedAt: '2018-03-20T16:38:07.045Z',
-    message: {
-      text: 'Hi it’s Freddie again! Just 4 more days until the March For Our Lives. I\'ll be attending the NYC march with the DoSomething staff and friends. Who will you march with? \n\nClick here to find a march near you and tag a friend who you want to invite: https://www.dosomething.org/us/campaigns/do-something-about-gun-violence/blocks/4grHyJPp60wecqmkOCG8UY?id={{user.id}}\n\n',
-      attachments: [],
-      template: 'rivescript',
-    },
-    topic: 'survey_response',
-  };
+/**
+ * @return {Object}
+ */
+function getValidTopicBroadcast(date = Date.now()) {
+  const broadcast = module.exports.getValidCampaignBroadcast(date);
+  broadcast.message.template = 'rivescript';
+  broadcast.topic = stubs.getTopic();
+  broadcast.campaignId = null;
+  return broadcast;
 }
 
 module.exports = {
