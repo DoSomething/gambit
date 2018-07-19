912b65348d7537e892c562b7ddc6b453e4f9a037
Aaron Schachter <aaronschachter@gmail.com>
2018-07-19 10:58:55 -0700

    Moves fetching defaultTopicTriggers into rivescript helper

--- config/lib/gambit-campaigns.js
+++ config/lib/gambit-campaigns.js
@@ -8,5 +8,6 @@ module.exports = {
   closedStatusValue: 'closed',
   endpoints: {
     broadcasts: 'broadcasts',
+    defaultTopicTriggers: 'defaultTopicTriggers',
   },
 };
--- test/unit/lib/lib-helpers/topic.test.js
+++ test/unit/lib/lib-helpers/topic.test.js
@@ -10,7 +10,6 @@ const gambitCampaigns = require('../../../../lib/gambit-campaigns');
 const helpers = require('../../../../lib/helpers');
 const stubs = require('../../../helpers/stubs');
 const campaignFactory = require('../../../helpers/factories/campaign');
-const defaultTopicTriggerFactory = require('../../../helpers/factories/defaultTopicTrigger');
 const topicFactory = require('../../../helpers/factories/topic');
 const config = require('../../../../config/lib/helpers/topic');
 
@@ -21,42 +20,11 @@ chai.use(sinonChai);
 const topicHelper = require('../../../../lib/helpers/topic');
 
 const hardcodedTopicId = config.hardcodedTopicIds[0];
-const replyTrigger = defaultTopicTriggerFactory.getValidReplyDefaultTopicTrigger();
-const redirectTrigger = defaultTopicTriggerFactory.getValidReplyDefaultTopicTrigger();
 
 const sandbox = sinon.sandbox.create();
 
 test.afterEach(() => {
   sandbox.restore();
-});
-
-// fetchAllDefaultTopicTriggers
-test('fetchAllDefaultTopicTriggers should call parseDefaultTopicTrigger on gambitCampaigns.fetchDefaultTopicTriggers success', async () => {
-  const mockResponse = [replyTrigger, redirectTrigger];
-  sandbox.stub(gambitCampaigns, 'fetchDefaultTopicTriggers')
-    .returns(Promise.resolve(mockResponse));
-  sandbox.stub(topicHelper, 'parseDefaultTopicTrigger')
-    .returns(replyTrigger);
-
-  const result = await topicHelper.fetchAllDefaultTopicTriggers();
-  mockResponse.forEach((item) => {
-    topicHelper.parseDefaultTopicTrigger.should.have.been.calledWith(item);
-  });
-  gambitCampaigns.fetchDefaultTopicTriggers.should.have.been.called;
-  result.should.deep.equal([replyTrigger, replyTrigger]);
-});
-
-test('fetchAllDefaultTopicTriggers should throw on gambitCampaigns.fetchDefaultTopicTriggers fail', async (t) => {
-  const mockError = new Error('epic fail');
-  sandbox.stub(gambitCampaigns, 'fetchDefaultTopicTriggers')
-    .returns(Promise.reject(mockError));
-  sandbox.stub(topicHelper, 'parseDefaultTopicTrigger')
-    .returns(replyTrigger);
-
-  const result = await t.throws(topicHelper.fetchAllDefaultTopicTriggers());
-  gambitCampaigns.fetchDefaultTopicTriggers.should.have.been.called;
-  topicHelper.parseDefaultTopicTrigger.should.not.have.been.called;
-  result.should.deep.equal(mockError);
 });
 
 // fetchById
@@ -125,27 +93,6 @@ test('isHardcodedTopicId should return whether topicId exists in config.hardcode
 test('isRandomTopicId should return whether topicId is config.randomTopicId', (t) => {
   t.truthy(topicHelper.isRandomTopicId(config.randomTopicId));
   t.falsy(topicHelper.isRandomTopicId(stubs.getContentfulId()));
-});
-
-// parseDefaultTopicTrigger
-test('parseDefaultTopicTrigger should return null if defaultTopicTrigger undefined', (t) => {
-  const result = topicHelper.parseDefaultTopicTrigger();
-  t.is(result, null);
-});
-
-test('parseDefaultTopicTrigger should return defaultTopicTrigger if defaultTopicTrigger.topicId undefined', () => {
-  const defaultTopicTrigger = defaultTopicTriggerFactory.getValidReplyDefaultTopicTrigger();
-  const result = topicHelper.parseDefaultTopicTrigger(defaultTopicTrigger);
-  result.should.deep.equal(defaultTopicTrigger);
-});
-
-test('parseDefaultTopicTrigger should return object with a changeTopic macro reply if defaultTopicTrigger.topicId', () => {
-  const mockChangeTopicMacro = `changeTopicTo${stubs.getTopicId()}`;
-  sandbox.stub(helpers.macro, 'getChangeTopicMacroFromTopicId')
-    .returns(mockChangeTopicMacro);
-  const defaultTopicTrigger = defaultTopicTriggerFactory.getValidChangeTopicDefaultTopicTrigger();
-  const result = topicHelper.parseDefaultTopicTrigger(defaultTopicTrigger);
-  result.reply.should.equal(mockChangeTopicMacro);
 });
 
 // getRenderedTextFromTopicAndTemplateName
