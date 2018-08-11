50601ac8fbc0a0d4671712072625be22987a5068
Aaron Schachter <aaronschachter@gmail.com>
2018-08-10 17:14:46 -0700

    Adds tests for helpers.replies.autoReply

--- test/unit/lib/lib-helpers/replies.test.js
+++ test/unit/lib/lib-helpers/replies.test.js
@@ -17,6 +17,7 @@ const Message = require('../../../../app/models/Message');
 const campaignFactory = require('../../../helpers/factories/campaign');
 const conversationFactory = require('../../../helpers/factories/conversation');
 const messageFactory = require('../../../helpers/factories/message');
+const topicFactory = require('../../../helpers/factories/topic');
 
 // setup "x.should.y" assertion style
 chai.should();
@@ -215,6 +216,57 @@ test('askSignup(): should call sendReplyWithTopicTemplate', async (t) => {
   await assertSendingReplyWithTopicTemplate(t.context.req, t.context.res, template);
 });
 
+// autoReply
+test('autoReply should call postCampaignActivityFromReq if topic has a campaign', async (t) => {
+  t.context.req.topic = topicFactory.getValidTextPostConfig();
+  sandbox.stub(helpers.request, 'postCampaignActivityFromReq')
+    .returns(Promise.resolve(gCampResponse.data));
+  sandbox.stub(repliesHelper, 'sendReplyWithTopicTemplate')
+    .returns(true);
+
+  await repliesHelper.autoReply(t.context.req, t.context.res);
+  helpers.request.postCampaignActivityFromReq.should.have.been.called;
+  repliesHelper.sendReplyWithTopicTemplate.should.have.been.called;
+});
+
+test('autoReply does not call postCampaignActivityFromReq if topic does not have campaign', async (t) => {
+  t.context.req.topic = topicFactory.getValidTopicWithoutCampaign();
+  sandbox.stub(helpers.request, 'postCampaignActivityFromReq')
+    .returns(Promise.resolve(gCampResponse.data));
+  sandbox.stub(repliesHelper, 'sendReplyWithTopicTemplate')
+    .returns(true);
+
+  await repliesHelper.autoReply(t.context.req, t.context.res);
+  helpers.request.postCampaignActivityFromReq.should.not.have.been.called;
+  repliesHelper.sendReplyWithTopicTemplate.should.have.been.called;
+});
+
+test('autoReply calls sendErrorResponse if postCampaignActivityFromReq fails', async (t) => {
+  t.context.req.topic = topicFactory.getValidTextPostConfig();
+  const mockError = { message: 'oh no' };
+  sandbox.stub(helpers.request, 'postCampaignActivityFromReq')
+    .returns(Promise.reject(mockError));
+  sandbox.stub(repliesHelper, 'sendReplyWithTopicTemplate')
+    .returns(true);
+
+  await repliesHelper.autoReply(t.context.req, t.context.res);
+  repliesHelper.sendReplyWithTopicTemplate.should.not.have.been.called;
+  helpers.sendErrorResponse.should.have.been.calledWith(t.context.res, mockError);
+});
+
+test('autoReply calls sendErrorResponse if sendReplyWithTopicTemplate fails', async (t) => {
+  t.context.req.topic = topicFactory.getValidTopicWithoutCampaign();
+  const mockError = { message: 'oh no' };
+  sandbox.stub(helpers.request, 'postCampaignActivityFromReq')
+    .returns(Promise.resolve(gCampResponse.data));
+  sandbox.stub(repliesHelper, 'sendReplyWithTopicTemplate')
+    .throws(mockError);
+
+  await repliesHelper.autoReply(t.context.req, t.context.res);
+  helpers.sendErrorResponse.should.have.been.calledWith(t.context.res, mockError);
+});
+
+// campaignClosed
 test('campaignClosed(): should call sendReplyWithTopicTemplate', async (t) => {
   const template = templates.campaignClosed;
   await assertSendingReplyWithTopicTemplate(t.context.req, t.context.res, template);
