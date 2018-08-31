e48350e64e2190e886cd16cb1e5e137aa65b6383
Aaron Schachter <aaronschachter@gmail.com>
2018-08-30 17:27:49 -0700

    Starts on lib/rivescript tests

---
+++ test/unit/lib/rivescript.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// libs
+require('dotenv').config();
+const test = require('ava');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const rewire = require('rewire');
+
+chai.should();
+chai.use(sinonChai);
+
+const sandbox = sinon.sandbox.create();
+
+const RiveScript = require('rivescript');
+
+const stubs = require('../../helpers/stubs');
+const config = require('../../../config/lib/rivescript');
+
+// Module to test
+const rivescript = rewire('../../../lib/rivescript');
+
+const userId = stubs.getUserId();
+const topicId = 'random';
+const messageText = stubs.getRandomMessageText();
+
+test.afterEach(() => {
+  sandbox.restore();
+  rivescript.__set__('brain', undefined);
+  rivescript.__set__('config', config);
+  rivescript.__set__('hasSortedReplies', false);
+});
+
+// getBot
+test('getBot should create a new Rivescript bot if brain undefined', () => {
+  const RiveScriptSpy = sandbox.spy();
+  rivescript.__set__('RiveScript', RiveScriptSpy);
+
+  rivescript.getBot();
+  RiveScriptSpy.should.have.been.calledWithNew;
+});
+
+test('getBot should return the existing Rivescript bot if already created', () => {
+  const RiveScriptSpy = sandbox.spy();
+  rivescript.__set__('RiveScript', RiveScriptSpy);
+  const newClient = rivescript.getBot();
+  const sameClient = rivescript.getBot();
+
+  // test
+  RiveScriptSpy.should.have.been.calledWithNew;
+  RiveScriptSpy.should.have.been.calledOnce;
+  newClient.should.be.equal(sameClient);
+});
+
+// getBotReply
+test('getBotReply should throw an error if brain undefined', async (t) => {
+  await t.throws(rivescript.getBotReply(userId, topicId, messageText));
+});
+
+test('getBotReply should throw an error if not hasSortedReplies', async (t) => {
+  rivescript.__set__('brain', new RiveScript());
+  await t.throws(rivescript.getBotReply(userId, topicId, messageText));
+});
