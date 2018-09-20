3634b34c9f6e83747f8a1f37aa658c4cb3ac5f37
Aaron Schachter <aaronschachter@gmail.com>
2018-09-20 11:48:20 -0700

    Adds test for twilioStudio response

--- test/unit/lib/middleware/messages/member/rivescript-reply-get.test.js
+++ test/unit/lib/middleware/messages/member/rivescript-reply-get.test.js
@@ -11,6 +11,8 @@ const underscore = require('underscore');
 
 const helpers = require('../../../../../../lib/helpers');
 const conversationFactory = require('../../../../../helpers/factories/conversation');
+const userFactory = require('../../../../../helpers/factories/user');
+const stubs = require('../../../../../helpers/stubs');
 
 const macroHelper = helpers.macro;
 const mockConversation = conversationFactory.getValidConversation();
@@ -22,6 +24,7 @@ const mockRivescriptReply = {
   topic: mockTopic,
   match: mockMatch,
 };
+const mockUser = userFactory.getValidUser();
 
 // setup "x.should.y" assertion style
 chai.should();
@@ -38,8 +41,9 @@ test.beforeEach((t) => {
     .returns(underscore.noop);
   t.context.req = httpMocks.createRequest();
   t.context.req.conversation = mockConversation;
-  t.context.req.inboundMessageText = 'King of the North';
+  t.context.req.inboundMessageText = stubs.getRandomMessageText();
   t.context.res = httpMocks.createResponse();
+  sandbox.spy(t.context.res, 'send');
 });
 
 test.afterEach((t) => {
@@ -124,4 +128,23 @@ test('getRivescriptReply should not set req.macro if helpers.macro.isMacro fails
   macroHelper.isMacro.should.have.been.called;
   next.should.not.have.been.called;
   helpers.sendErrorResponse.should.have.been.called;
+});
+
+test('getRivescriptReply should send data with user and reply if request is twilioStudio', async (t) => {
+  // setup
+  const next = sinon.stub();
+  const middleware = getRivescriptReply();
+  sandbox.stub(helpers.request, 'getRivescriptReply')
+    .returns(Promise.resolve(mockRivescriptReply));
+  sandbox.stub(helpers.request, 'isTwilioStudio')
+    .returns(true);
+  t.context.req.user = mockUser;
+  const data = {
+    user: mockUser,
+    reply: mockRivescriptReply,
+  };
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+  t.context.res.send.should.have.been.calledWith({ data });
 });
