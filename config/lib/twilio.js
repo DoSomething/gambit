4dd4bf1845343d8e160054b06b480c81d4168f12
Rafa <rapala61@gmail.com>
2018-09-24 19:26:33 -0500

    removes Twilio testToNumber since Twilio test API does validate this number so we need to send real numbers.

--- config/lib/twilio.js
+++ config/lib/twilio.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const useTwilioTestCreds = process.env.DS_GAMBIT_CONVERSATIONS_USE_TWILIO_TEST_CREDS || 'false';
+let useTwilioTestCreds = process.env.DS_GAMBIT_CONVERSATIONS_USE_TWILIO_TEST_CREDS || 'false';
+
+/**
+ * TODO: We should be overriding this in the config/env/override-test.js,
+ * but since it only override basic app variables it wouldn't work so we do it here.
+ * It would be a good idea to import all configs into the main config/index.js and require
+ * that everywhere, this way we could override any config through the env override files.
+ */
+if (process.env.NODE_ENV === 'test') {
+  useTwilioTestCreds = 'true';
+}
+
 const accountSid = useTwilioTestCreds === 'true' ?
   process.env.TWILIO_TEST_ACCOUNT_SID :
   process.env.TWILIO_ACCOUNT_SID;
@@ -10,15 +21,12 @@ const authToken = useTwilioTestCreds === 'true' ?
 
 // @see https://www.twilio.com/docs/api/rest/test-credentials#test-sms-messages-parameters-From
 const testFromNumber = process.env.TWILIO_TEST_FROM_NUMBER || '+15005550006';
-// @see https://www.twilio.com/docs/api/rest/test-credentials#test-sms-messages-parameters-To
-const testToNumber = process.env.TWILIO_TEST_TO_NUMBER || '+15005550006';
 
 module.exports = {
   accountSid,
   authToken,
   fromNumber: process.env.TWILIO_FROM_NUMBER,
   testFromNumber,
-  testToNumber,
   useTwilioTestCreds,
   statusCallbackUrl: process.env.TWILIO_STATUS_CALLBACK_URL,
 };
