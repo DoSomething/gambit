b9917d3a8497a1a43db0f9dc6efca85347aa7157
Aaron Schachter <aaronschachter@gmail.com>
2018-09-29 11:24:11 -0700

    DRY macro names

--- config/lib/helpers/user.js
+++ config/lib/helpers/user.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const macros = require('./macro').macros;
 // TODO: Deprecate subscription config by defining profile field values in this file, as we'll soon
 // be adding more profile fields/values to update via new macros.
 const subscriptionStatusValues = require('./subscription').subscriptionStatuses;
@@ -16,18 +17,21 @@ function getSubscriptionStatusUpdate(value) {
   return payload;
 }
 
+const updatesByMacro = {};
+/* eslint-disable max-len */
+updatesByMacro[macros.subscriptionStatusActive] = getSubscriptionStatusUpdate(subscriptionStatusValues.active);
+updatesByMacro[macros.subscriptionStatusLess] = getSubscriptionStatusUpdate(subscriptionStatusValues.less);
+updatesByMacro[macros.subscriptionStatusResubscribed] = getSubscriptionStatusUpdate(subscriptionStatusValues.active);
+updatesByMacro[macros.subscriptionStatusStop] = getSubscriptionStatusUpdate(subscriptionStatusValues.stop);
+// TODO: Add more macros to save values to voting plan fields
+// e.g. votingPlanStatusVoting, votingPlanAttendingWithFriends
+/* eslint-enable max-len */
+
 module.exports = {
   createOptions: {
     passwordAlgorithm: 'sha1',
     passwordKey: process.env.DS_GAMBIT_CREATE_USER_PASSWORD_KEY || 'puppetSlothForever',
     passwordLength: 6,
   },
-  updatesByMacro: {
-    subscriptionStatusActive: getSubscriptionStatusUpdate(subscriptionStatusValues.active),
-    subscriptionStatusLess: getSubscriptionStatusUpdate(subscriptionStatusValues.less),
-    subscriptionStatusResubscribed: getSubscriptionStatusUpdate(subscriptionStatusValues.active),
-    subscriptionStatusStop: getSubscriptionStatusUpdate(subscriptionStatusValues.stop),
-    // TODO: Add more macros to save values to voting plan fields
-    // e.g. votingPlanStatusVoting, votingPlanAttendingWithFriends
-  },
+  updatesByMacro,
 };
