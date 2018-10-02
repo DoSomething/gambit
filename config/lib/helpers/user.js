1110aa36f3ee06246c45b14db7d6b5a5e98a391c
Aaron Schachter <aaronschachter@gmail.com>
2018-10-02 09:40:42 -0700

    Undo moving things for less tracked changes

--- config/lib/helpers/user.js
+++ config/lib/helpers/user.js
@@ -1,32 +1,31 @@
 'use strict';
 
+// TODO: Move subscription config into this file to deprecate.
+const subscriptionStatusValues = require('./subscription').subscriptionStatuses;
+
+const userFields = {
+  // Note: All conversations save subscription status as user sms_status for now: sms is the only
+  // supported platform where users message us (Slack is internal to staff via gambit-slack app)
+  subscriptionStatus: {
+    name: 'sms_status',
+    values: subscriptionStatusValues,
+  },
+  votingPlanStatus: {
+    name: 'voting_plan_status',
+    values: {
+      cantVote: 'cant_vote',
+      notVoting: 'not_voting',
+      voted: 'voted',
+      voting: 'voting',
+    },
+  },
+};
+
 module.exports = {
   createOptions: {
     passwordAlgorithm: 'sha1',
     passwordKey: process.env.DS_GAMBIT_CREATE_USER_PASSWORD_KEY || 'puppetSlothForever',
     passwordLength: 6,
   },
-  fields: {
-    // Note: All conversations save subscription status to sms_status field for now: sms is the only
-    // supported platform where users message us (Slack is internal to staff via gambit-slack app)
-    subscriptionStatus: {
-      name: 'sms_status',
-      values: {
-        active: 'active',
-        less: 'less',
-        pending: 'pending',
-        stop: 'stop',
-        undeliverable: 'undeliverable',
-      },
-    },
-    votingPlanStatus: {
-      name: 'voting_plan_status',
-      values: {
-        cantVote: 'cant_vote',
-        notVoting: 'not_voting',
-        voted: 'voted',
-        voting: 'voting',
-      },
-    },
-  },
+  fields: userFields,
 };
