cf4d5ed7ec68f9591b7d621f222c841375dd5c76
Aaron Schachter <aaronschachter@gmail.com>
2018-10-10 16:04:09 -0700

    Adds tests for invalidVotingStatusVoting, votingPlanStatusVoted

--- test/unit/lib/middleware/messages/member/catchAll-askVotingPlanStatus.test.js
+++ test/unit/lib/middleware/messages/member/catchAll-askVotingPlanStatus.test.js
@@ -10,6 +10,7 @@ const httpMocks = require('node-mocks-http');
 const underscore = require('underscore');
 
 const helpers = require('../../../../../../lib/helpers');
+const stubs = require('../../../../../helpers/stubs');
 const broadcastFactory = require('../../../../../helpers/factories/broadcast');
 const topicFactory = require('../../../../../helpers/factories/topic');
 
@@ -21,6 +22,7 @@ const askVotingPlanStatusCatchAll = require('../../../../../../lib/middleware/me
 
 // stubs
 const askVotingPlanStatus = broadcastFactory.getValidAskVotingPlanStatus();
+const messageText = stubs.getRandomMessageText();
 const error = { message: 'Epic fail' };
 
 const sandbox = sinon.sandbox.create();
@@ -28,6 +30,8 @@ const sandbox = sinon.sandbox.create();
 test.beforeEach((t) => {
   sandbox.stub(helpers.replies, 'sendReply')
     .returns(underscore.noop);
+  sandbox.stub(helpers.request, 'changeTopic')
+    .returns(Promise.resolve(true));
   sandbox.stub(helpers, 'sendErrorResponse')
     .returns(underscore.noop);
   t.context.req = httpMocks.createRequest();
@@ -72,3 +76,55 @@ test('askVotingPlanStatusCatchAll should call sendErrorResponse if parseAskVotin
   helpers.sendErrorResponse.should.have.been.calledWith(t.context.res, error);
 });
 
+test('askVotingPlanStatusCatchAll should not call changeTopic, should call sendReply if macro isInvalidVotingPlanStatus', async (t) => {
+  const next = sinon.stub();
+  const macro = 'invalidVotingPlanStatus';
+  const middleware = askVotingPlanStatusCatchAll();
+  sandbox.stub(helpers.request, 'parseAskVotingPlanStatusResponse')
+    .returns(Promise.resolve(true));
+  sandbox.stub(helpers.topic, 'isAskVotingPlanStatus')
+    .returns(true);
+  sandbox.stub(helpers.macro, 'isInvalidVotingPlanStatus')
+    .returns(true);
+  sandbox.stub(helpers.macro, 'getMacro')
+    .returns({ text: messageText });
+  t.context.req.topic = askVotingPlanStatus;
+  t.context.req.macro = macro;
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+
+  helpers.topic.isAskVotingPlanStatus.should.have.been.calledWith(t.context.req.topic);
+  next.should.not.have.been.called;
+  helpers.request.changeTopic.should.not.have.been.called;
+  helpers.replies.sendReply
+    .should.have.been.calledWith(t.context.req, t.context.res, messageText, macro);
+});
+
+test('askVotingPlanStatusCatchAll should call changeTopic and sendReply if macro isVotingPlanStatusVoting', async (t) => {
+  const next = sinon.stub();
+  const macro = 'votingPlanStatusVoting';
+  const middleware = askVotingPlanStatusCatchAll();
+  const topic = topicFactory.getValidAutoReply();
+  sandbox.stub(helpers.request, 'parseAskVotingPlanStatusResponse')
+    .returns(Promise.resolve(true));
+  sandbox.stub(helpers.topic, 'isAskVotingPlanStatus')
+    .returns(true);
+  sandbox.stub(helpers.macro, 'isInvalidVotingPlanStatus')
+    .returns(false);
+  sandbox.stub(helpers.macro, 'isVotingPlanStatusVoting')
+    .returns(true);
+  sandbox.stub(helpers.macro, 'getMacro')
+    .returns({ text: messageText, topic });
+  t.context.req.topic = askVotingPlanStatus;
+  t.context.req.macro = macro;
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+
+  helpers.topic.isAskVotingPlanStatus.should.have.been.calledWith(t.context.req.topic);
+  next.should.not.have.been.called;
+  helpers.request.changeTopic.should.have.been.calledWith(t.context.req, topic);
+  helpers.replies.sendReply
+    .should.have.been.calledWith(t.context.req, t.context.res, messageText, macro);
+});
