9a73e07396495b3cee6a2037ac5ac2ff918b2321
Aaron Schachter <aaronschachter@gmail.com>
2018-10-30 16:23:38 -0700

    Begins refactoring custom_url as links

--- config/lib/helpers/tags.js
+++ config/lib/helpers/tags.js
@@ -1,26 +1,27 @@
 'use strict';
 
+const pollingLocatorQuery = {
+  source: 'sms',
+  utm_campaign: 'sms_gotv',
+  utm_medium: 'sms',
+  utm_source: 'dosomething',
+};
+
 module.exports = {
   // Tags used in message templates.
   tags: {
-    customUrl: 'custom_url',
+    links: 'links',
     user: 'user',
   },
-  /**
-   * The customUrl tag should return a custom link with Conversation variables set in query string:
-   * e.g. http://ds.puppetsloth.mx?referral-code=user:5745b207469c69618b46d97,campaign:97,source:sms
-   */
-  customUrl: {
-    url: process.env.DS_GAMBIT_CUSTOM_URL || 'https://dosomething.turbovote.org',
-    queryParamName: process.env.DS_GAMBIT_CUSTOM_URL_QUERY_PARAM || 'r',
-    queryValue: {
-      separator: ',',
-      fieldSuffix: ':',
-      fields: {
-        userId: 'user',
-        campaignId: 'campaignID',
-        campaignRunId: 'campaignRunID',
-        platform: 'source',
+  links: {
+    pollingLocator: {
+      find: {
+        url: 'https://www.dosomething.org/us/polling-locator-2018',
+        query: pollingLocatorQuery,
+      },
+      share: {
+        url: 'https://www.dosomething.org/us/campaigns/find-your-v-spot/blocks/5WuCqMMGre02mq8MqK4co6',
+        query: pollingLocatorQuery,
       },
     },
   },
