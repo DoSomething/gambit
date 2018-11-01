078b7d64210d903ea0a85a35fd47ac565db51553
Aaron Schachter <aaronschachter@gmail.com>
2018-11-01 10:26:22 -0700

    Cleanup askVotingPlanAttendingWith text

--- config/lib/helpers/macro.js
+++ config/lib/helpers/macro.js
@@ -12,7 +12,7 @@ const askSubscriptionStatusText = 'Do you want texts: A)Weekly B)Monthly C)I nee
 const newsUrl = 'https://www.dosomething.org/us/spot-the-signs-guide?source=sms&utm_source=dosomething&utm_medium=sms&utm_campaign=permissioning_weekly&user_id={{user.id}}';
 
 // Voting plan.
-const askVotingPlanAttendingWithText = process.env.DS_GAMBIT_CONVERSATIONS_ASK_VOTING_PLAN_ATTENDING_WITH_TEXT || 'Who are you planning on voting with A) Alone B) Friends C) Family D) Co-workers';
+const askVotingPlanAttendingWithText = process.env.DS_GAMBIT_CONVERSATIONS_ASK_VOTING_PLAN_ATTENDING_WITH_TEXT || 'Who are you planning on voting with? A) Alone B) Friends C) Family D) Co-workers';
 const askVotingPlanMethodOfTransportText = process.env.DS_GAMBIT_CONVERSATIONS_ASK_VOTING_PLAN_METHOD_OF_TRANSPORT_TEXT || 'How are you planning on getting to the polls? A) Drive B) Walk C) Bike D) Public transportation';
 const askVotingPlanStatusText = process.env.DS_GAMBIT_CONVERSATIONS_ASK_VOTING_PLAN_STATUS_TEXT || 'Are you planning on voting? A) Yes B) No C) Already voted D) Can\'t vote';
 const askVotingPlanTimeOfDayText = process.env.DS_GAMBIT_CONVERSATIONS_ASK_VOTING_PLAN_TIME_OF_DAY_TEXT || 'What time are you planning on voting? A) Morning B) Afternoon C) Evening';
@@ -34,6 +34,14 @@ const completedVotingPlanText = process.env.DS_GAMBIT_CONVERSATIONS_COMPLETED_VO
  */
 function macroName(prefix, valueKey) {
   return `${prefix}${valueKey.charAt(0).toUpperCase() + valueKey.slice(1)}`;
+}
+
+/**
+ * @param {String} string
+ * @return {String}
+ */
+function lowercaseFirstLetter(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 /**
@@ -78,7 +86,7 @@ function votingPlanMethodOfTransport(valueKey) {
   return {
     name: macroName('votingPlanMethodOfTransport', valueKey),
     // After saving method of transport, ask for attending with.
-    text: `And finally, ${askVotingPlanAttendingWithText}`,
+    text: `And finally, ${lowercaseFirstLetter(askVotingPlanAttendingWithText)}`,
     topic: rivescriptTopics.askVotingPlanAttendingWith,
     profileUpdate: {
       field: profile.votingPlanMethodOfTransport.name,
