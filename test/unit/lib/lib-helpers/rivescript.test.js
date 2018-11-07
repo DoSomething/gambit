75a2ef5c9002bc9c3931269f8a81d07564507ea0
Aaron Schachter <aaronschachter@gmail.com>
2018-11-06 20:10:05 -0800

    Adds test for formatRedirectRivescript

--- test/unit/lib/lib-helpers/rivescript.test.js
+++ test/unit/lib/lib-helpers/rivescript.test.js
@@ -149,6 +149,17 @@ test('fetchRivescripts should throw on gambitCampaigns.fetchDefaultTopicTriggers
   result.should.deep.equal(mockError);
 });
 
+// formatRedirectRivescript
+test('formatRedirectRivescript should return redirect command with text arg as rs line', () => {
+  sandbox.stub(rivescriptHelper, 'formatRivescriptLine')
+    .returns(mockRivescriptLine);
+
+  const result = rivescriptHelper.formatRedirectRivescript(mockWord);
+  rivescriptHelper.formatRivescriptLine
+    .should.have.been.calledWith(config.commands.redirect, mockWord);
+  result.should.equal(mockRivescriptLine);
+});
+
 // formatReplyRivescript
 test('formatReplyRivescript return array of Rivescript lines', () => {
   const data = [
