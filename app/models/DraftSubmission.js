9fbd7ae4a9636c63d82a5f67b6268d2f591d6626
Aaron Schachter <aaronschachter@gmail.com>
2018-11-16 12:22:05 -0800

    WIP: adds DraftSubmission model

---
+++ app/models/DraftSubmission.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+/**
+ * Schema.
+ */
+const draftSubmissionSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+  },
+  topicId: String,
+  data: Object,
+}, {
+  collection: 'draftSubmissions',
+  timestamps: true,
+});
+
+draftSubmissionSchema.index({
+  conversationId: 1,
+  topicId: 1,
+}, {
+  unique: true,
+});
+
+module.exports = mongoose.model('DraftSubmission', draftSubmissionSchema);
