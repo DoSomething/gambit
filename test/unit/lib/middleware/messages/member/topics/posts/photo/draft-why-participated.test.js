f55a66151971cfd8716e37bb9f1d505a26e169ae
Aaron Schachter <aaronschachter@gmail.com>
2018-11-28 22:45:26 -0800

    Starts on draft-why-participated tests

--- test/unit/lib/middleware/messages/member/topics/posts/photo/draft-why-participated.test.js
+++ test/unit/lib/middleware/messages/member/topics/posts/photo/draft-why-participated.test.js
@@ -16,7 +16,7 @@ chai.should();
 chai.use(sinonChai);
 
 // module to be tested
-const draftCaption = require('../../../../../../../../../lib/middleware/messages/member/topics/posts/photo/draft-caption');
+const draftWhyParticipated = require('../../../../../../../../../lib/middleware/messages/member/topics/posts/photo/draft-why-participated');
 
 const sandbox = sinon.sandbox.create();
 
@@ -25,7 +25,7 @@ test.beforeEach((t) => {
     .returns(underscore.noop);
   sandbox.stub(helpers.replies, 'askWhyParticipated')
     .returns(underscore.noop);
-  sandbox.stub(helpers.replies, 'invalidCaption')
+  sandbox.stub(helpers.replies, 'invalidWhyParticipated')
     .returns(underscore.noop);
   t.context.req = httpMocks.createRequest();
   t.context.res = httpMocks.createResponse();
@@ -36,9 +36,9 @@ test.afterEach((t) => {
   t.context = {};
 });
 
-test('draftCaption should call next if topic is not photo post', async (t) => {
+test('draftWhyParticipated should call next if topic is not photo post', async (t) => {
   const next = sinon.stub();
-  const middleware = draftCaption();
+  const middleware = draftWhyParticipated();
   sandbox.stub(helpers.topic, 'isPhotoPostConfig')
     .returns(false);
 
@@ -48,14 +48,34 @@ test('draftCaption should call next if topic is not photo post', async (t) => {
   helpers.topic.isPhotoPostConfig.should.have.been.calledWith(t.context.req.topic);
   next.should.have.been.called;
   helpers.replies.askWhyParticipated.should.not.have.been.called;
-  helpers.replies.invalidCaption.should.not.have.been.called;
+  helpers.replies.invalidWhyParticipated.should.not.have.been.called;
 });
 
-test('draftCaption should call next if request hasDraftSubmissionValue', async (t) => {
+test('draftWhyParticipated should call next if request hasSignupWithWhyParticipated', async (t) => {
   const next = sinon.stub();
-  const middleware = draftCaption();
+  const middleware = draftWhyParticipated();
   sandbox.stub(helpers.topic, 'isPhotoPostConfig')
     .returns(true);
+  sandbox.stub(helpers.request, 'hasSignupWithWhyParticipated')
+    .returns(Promise.resolve(true));
+
+  // test
+  await middleware(t.context.req, t.context.res, next);
+
+  helpers.topic.isPhotoPostConfig.should.have.been.calledWith(t.context.req.topic);
+  helpers.request.hasSignupWithWhyParticipated.should.have.been.calledWith(t.context.req);
+  next.should.have.been.called;
+  helpers.replies.askWhyParticipated.should.not.have.been.called;
+  helpers.replies.invalidWhyParticipated.should.not.have.been.called;
+});
+
+test('draftWhyParticipated should call next if request not hasSignupWithWhyParticipated and request hasDraftSubmissionValue', async (t) => {
+  const next = sinon.stub();
+  const middleware = draftWhyParticipated();
+  sandbox.stub(helpers.topic, 'isPhotoPostConfig')
+    .returns(true);
+  sandbox.stub(helpers.request, 'hasSignupWithWhyParticipated')
+    .returns(Promise.resolve(false));
   sandbox.stub(helpers.request, 'hasDraftSubmissionValue')
     .returns(Promise.resolve(true));
 
@@ -63,15 +83,16 @@ test('draftCaption should call next if request hasDraftSubmissionValue', async (
   await middleware(t.context.req, t.context.res, next);
 
   helpers.topic.isPhotoPostConfig.should.have.been.calledWith(t.context.req.topic);
+  helpers.request.hasSignupWithWhyParticipated.should.have.been.calledWith(t.context.req);
   helpers.request.hasDraftSubmissionValue.should.have.been.calledWith(t.context.req);
   next.should.have.been.called;
   helpers.replies.askWhyParticipated.should.not.have.been.called;
-  helpers.replies.invalidCaption.should.not.have.been.called;
+  helpers.replies.invalidWhyParticipated.should.not.have.been.called;
 });
 
-test('draftCaption should call sendErrorResponse if error is caught', async (t) => {
+test('draftWhyParticipated should call sendErrorResponse if error is caught', async (t) => {
   const next = sinon.stub();
-  const middleware = draftCaption();
+  const middleware = draftWhyParticipated();
   const error = stubs.getError();
   sandbox.stub(helpers.topic, 'isPhotoPostConfig')
     .throws(error);
