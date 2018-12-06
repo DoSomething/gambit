dc5207395c5dd832ba120acedadd0a184e1a63ff
Rafa <rapala61@gmail.com>
2018-12-06 15:07:04 -0500

    better comments for redis client event handlers

--- config/redis.js
+++ config/redis.js
@@ -15,17 +15,20 @@ let redisClient;
 module.exports = function getRedisClient() {
   if (!redisClient) {
     redisClient = redis.createClient(redisUrl);
+    // @see https://www.npmjs.com/package/redis#error
     redisClient.on('error', (error) => {
       logger.error(`redisClient connection error: ${error}`);
       redisClient.quit();
       throw error;
     });
+    // @see https://www.npmjs.com/package/redis#reconnecting
     redisClient.on('reconnecting', () => {
       logger.debug('redisClient is reconnecting');
     });
     /**
-     * Set the connection name every time it reconnects
-     * otherwise  the name is lost on reconnection
+     * Set the client name every time it reconnects
+     * otherwise the new connection's name will be empty
+     * @see https://www.npmjs.com/package/redis#ready
      */
     redisClient.on('ready', () => {
       // Save the worker pid as the client name to help id connections
