636a7cf6696ee58d90ad3e71adc998dc7b6a9c05
Rafa <rapala61@gmail.com>
2018-12-07 17:08:46 -0500

    Moved newrelic initialization to main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 // Load environment vars.
 require('dotenv').config();
 
+// @see https://docs.newrelic.com/docs/agents/nodejs-agent/installation-configuration
+require('newrelic');
+
 const cluster = require('./lib/cluster');
 
 if (cluster.clusterRef.isMaster) {
