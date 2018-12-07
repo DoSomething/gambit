1d0b3bc4e7a79ac36a5b429552627f79db5218d2
Rafa <rapala61@gmail.com>
2018-12-07 13:13:58 -0500

    refactoring processes.js to make it more testable

--- processes.js
+++ processes.js
@@ -1,39 +1,12 @@
 'use strict';
 
-/* eslint-disable global-require */
 // Load environment vars.
 require('dotenv').config();
 
-const cluster = require('cluster');
+const cluster = require('./lib/cluster');
 
-const config = require('./config');
-const logger = require('./lib/logger');
-/**
- * exitHandler - When any of the workers die the cluster module will emit the 'exit' event.
- *
- * @see https://nodejs.org/docs/latest-v8.x/api/cluster.html#cluster_event_exit_1
- * @param {cluster.Worker} worker
- * @param {Number} code
- * @param {String} signal
- */
-function exitHandler(worker, code, signal) {
-  logger.info(`Worker ${worker.process.pid} died. Restarting...`, {
-    errorCode: code,
-    killSignal: signal,
-  });
-  // Respawn a worker when it dies
-  cluster.fork();
-}
-
-if (cluster.isMaster) {
-  logger.debug('Master process');
-  // Fork workers
-  for (let workers = 0; workers < config.processes.total; workers++) { // eslint-disable-line
-    cluster.fork();
-  }
-  // Register exit handler
-  cluster.on('exit', exitHandler);
+if (cluster.clusterRef.isMaster) {
+  cluster.masterProcessInit();
 } else {
-  // Init worker
-  require('./server')();
+  cluster.workerProcessInit();
 }
