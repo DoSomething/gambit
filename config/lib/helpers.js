61d7ffc4297322fe5a425a08c9635ee1bc117a40
Rafa <rapala61@gmail.com>
2019-01-29 15:45:29 -0500

    chore (renaming): cleanup

--- config/lib/helpers.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-  blinkSuppressHeaders: 'x-blink-retry-suppress',
-};
---
+++ config/lib/helpers/response.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  noRetryHeader: 'x-blink-retry-suppress',
+};
