938d3b897ee6f73b5a6862fc1ed024ade500d3d9
Rafa <rapala61@gmail.com>
2019-03-01 12:03:57 -0500

    tests: Added actionId to photo and text topics in the topic factory

--- test/helpers/factories/topic.js
+++ test/helpers/factories/topic.js
@@ -8,7 +8,13 @@ function getTemplate() {
 }
 
 /**
- * These topic stubs correspond to data returned from Query.topic GraphQL requests.
+ * TODO: Let's figure out a way to utilize the queries setup in config/lib/graphql.
+ * Managing this factory completely separate from where we defined the expected
+ * topic schema is a hard job!
+ *
+ * @see https://www.apollographql.com/docs/graphql-tools/mocking.html Probably?
+ *
+ * These topic stubs correspond to data returned from Query.topic GraphQL requests.s
  * @see https://github.com/DoSomething/gambit-content/blob/master/documentation/endpoints/topics.md#retrieve-topic
  *
  * @param {String} type
@@ -50,24 +56,28 @@ function getValidAutoReply() {
  * @return {Object}
  */
 function getValidPhotoPostConfig() {
-  return getValidTopic(config.types.photoPostConfig.type, {
+  const topic = getValidTopic(config.types.photoPostConfig.type, {
     startPhotoPostAutoReply: getTemplate(),
     askQuantity: getTemplate(),
     invalidQuantity: getTemplate(),
     // TODO: Cleanup config/lib/helpers/template, possibly move into config/lib/helpers/topic
     // and use it to define the various templates per topic here.
   });
+  topic.actionId = stubs.getRandomNumericId();
+  return topic;
 }
 
 /**
  * @return {Object}
  */
 function getValidTextPostConfig() {
-  return getValidTopic(config.types.textPostConfig.type, {
+  const topic = getValidTopic(config.types.textPostConfig.type, {
     invalidAskText: getTemplate(),
     completedTextPost: getTemplate(),
     completedTextPostAutoReply: getTemplate(),
   });
+  topic.actionId = stubs.getRandomNumericId();
+  return topic;
 }
 
 /**
