36425cde83d7f6952a775542f6728a0e6989d2fd
Chloe Lee <chloealessandra@gmail.com>
2019-06-03 16:58:25 -0400

    WIP

--- test/helpers/integration/routes.js
+++ test/helpers/integration/routes.js
@@ -71,7 +71,7 @@ module.exports = {
   },
   bertly: {
     baseURI: bertlyConfig.baseUri,
-    intercept: function intercept(urlPath, reply = {}, times = 1, status = 200) {
+    intercept: function intercept(reply = {}, times = 1, status = 200) {
       return nock(this.baseURI)
         .post()
         .times(times)
