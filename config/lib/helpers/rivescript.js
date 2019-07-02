4d7aae99109975cc7405265bfec47d99d7f61a5d
Rafa <rapala61@gmail.com>
2019-07-02 17:34:37 -0500

    feature: Adds defaultTopicTriggerTransformer to rivescript helper. Cleans up broadcast and topic helper transformers

--- config/lib/helpers/rivescript.js
+++ config/lib/helpers/rivescript.js
@@ -4,6 +4,15 @@
  * @see @see https://www.rivescript.com/docs/tutorial#the-code-explained
  */
 module.exports = {
+  // Used to determine how to transform defaultTrigger response contentTypes
+  // when fetching from GraphQl
+  response: {
+    types: {
+      askMultipleChoice: {
+        type: 'askMultipleChoice',
+      },
+    },
+  },
   cacheKey: 'contentApi',
   commands: {
     // A continuation is needed if a reply contains line breaks.
