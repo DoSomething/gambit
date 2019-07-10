8f9d70cf05b45369d2dce09a987ad58c37ba50aa
Rafa <rapala61@gmail.com>
2019-07-09 20:30:20 -0500

    chore: fix alphabetical ordering of properties per https://github.com/DoSomething/gambit/pull/505#discussion_r301770143

--- config/lib/helpers/rivescript.js
+++ config/lib/helpers/rivescript.js
@@ -4,15 +4,6 @@
  * @see @see https://www.rivescript.com/docs/tutorial#the-code-explained
  */
 module.exports = {
-  // Used to determine how to transform defaultTrigger response contentTypes
-  // when fetching from GraphQl
-  response: {
-    types: {
-      askMultipleChoice: {
-        type: 'askMultipleChoice',
-      },
-    },
-  },
   cacheKey: 'contentApi',
   commands: {
     // A continuation is needed if a reply contains line breaks.
@@ -22,6 +13,15 @@ module.exports = {
     redirect: '@',
     reply: '-',
     trigger: '+',
+  },
+  // Used to determine how to transform defaultTrigger response contentTypes
+  // when fetching from GraphQl
+  response: {
+    types: {
+      askMultipleChoice: {
+        type: 'askMultipleChoice',
+      },
+    },
   },
   separators: {
     command: ' ',
