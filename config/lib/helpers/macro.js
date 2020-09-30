b4402f7b41a1bfd042cd8eb58e84a5577f9eab72
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-09-30 13:05:07 -0400

    Update compliance/welcome copy to new SMS voice cohort
    With Tej's departure, we've migrated to a new cohort of SMS voices.

--- config/lib/helpers/macro.js
+++ config/lib/helpers/macro.js
@@ -7,7 +7,7 @@ const defaultTopic = rivescriptTopics.default;
 const invalidAnswerText = 'Sorry, I didn\'t get that.';
 
 // Subscription status.
-const activeSubscriptionStatusText = process.env.DS_GAMBIT_CONVERSATIONS_SUBSCRIPTION_STATUS_ACTIVE_TEXT || '👋 Hi I\'m Tej from DoSomething.org! I\'ll be txting you weekly, so get to know me! (Msg&DataRatesApply. Txt HELP for help, STOP to stop)\n\nhttps://www.dosomething.org/us/articles/6-things-you-should-know-about-me-tej?utm_source=content_fun&utm_medium=sms&utm_campaign=sms_compliance_message&user_id={{user.id}}';
+const activeSubscriptionStatusText = process.env.DS_GAMBIT_CONVERSATIONS_SUBSCRIPTION_STATUS_ACTIVE_TEXT || '👋 Welcome to DoSomething.org! Meet the staffers who\'ll be texting you: https://www.dosomething.org/us/articles/meet-the-staff-sms?user_id={{user.id}}&utm_campaign=sms_compliance_message&utm_medium=sms&utm_source=content_funMsg&DataRatesApply. Txt HELP for help, STOP to stop';
 const lessSubscriptionStatusText = 'Great, you\'ll start to receive 1 monthly update from Tej at DoSomething.org! Things to know: Msg&DataRatesApply. Text HELP for help, text STOP to stop.';
 const stopSubscriptionStatusText = process.env.DS_GAMBIT_CONVERSATIONS_SUBSCRIPTION_STATUS_STOP_TEXT || "You're unsubscribed from DoSomething.org Alerts. No more msgs will be sent. Text JOIN to receive 4-8 msgs/mth.\n\nLeave your feedback: https://dosomething.typeform.com/to/DHWcen?user_id={{user.id}}";
 
