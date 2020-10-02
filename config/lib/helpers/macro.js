01d5c72c29fffa30845e0d4a1d5cf28f814d0faa
Aaron Schachter <aaronschachter@gmail.com>
2020-10-02 16:24:54 -0700

    Lint fix

--- config/lib/helpers/macro.js
+++ config/lib/helpers/macro.js
@@ -13,7 +13,7 @@ const activeSubscriptionStatusText = '👋 Welcome to DoSomething.org! Meet the 
 const lessSubscriptionStatusText = 'Great, you\'ll start to receive 1 monthly update from DoSomething.org! Things to know: Msg&DataRatesApply. Text HELP for help, text STOP to stop.';
 const stopSubscriptionStatusText = 'You\'re unsubscribed from DoSomething.org Alerts. No more msgs will be sent. Text JOIN to receive 4-8 msgs/mth.\n\nLeave your feedback: https://dosomething.typeform.com/to/DHWcen?user_id={{user.id}}';
 // Prompt for voting method field value.
-const askVotingMethodText =  'How do you plan on voting? A) In person B) Vote By Mail{{#user.earlyVotingStarts}} C) Early Voting{{/user.earlyVotingStarts}}';
+const askVotingMethodText = 'How do you plan on voting? A) In person B) Vote By Mail{{#user.earlyVotingStarts}} C) Early Voting{{/user.earlyVotingStarts}}';
 // Prompt for voting plan field values.
 const askVotingPlanAttendingWithText = 'Who are you planning on voting with? A) Alone B) Friends C) Family D) Co-workers';
 const askVotingPlanMethodOfTransportText = 'How are you planning on getting to the polls? A) Drive B) Walk C) Bike D) Public transportation';
