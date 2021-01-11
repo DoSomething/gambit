247fb9a1f3e97898b5c4c4233af311340d00e45b
Aaron Schachter <aaronschachter@gmail.com>
2021-01-11 15:05:27 -0800

    Adds replies for asking hours spent

--- config/lib/helpers/replies.js
+++ config/lib/helpers/replies.js
@@ -10,6 +10,10 @@ const supportText = `Text ${commands.support} if you have a question.`;
 
 // Static Templates
 module.exports = {
+  askHoursSpent: {
+    name: 'askHoursSpent',
+    text: 'How many hours did this action take?'
+  },
   badWords: {
     name: 'badWords',
     text: `Not cool. I'm a real person & that offends me. I send out these texts to help young ppl take action. If you don't want my texts, text ${commands.stop} or ${commands.less} to get less.`,
@@ -17,6 +21,10 @@ module.exports = {
   campaignClosed: {
     name: 'campaignClosed',
     text: process.env.GAMBIT_CONVERSATIONS_CAMPAIGN_CLOSED_TEXT || `Sorry, this campaign is no longer available. ${supportText}`,
+  },
+  invalidHoursSpent: {
+    name: 'invalidHoursSpent',
+    text: `Whoops, I didn't understand that. How many hours did this action take? Be sure to text in a number, not a word (i.e. “4”, not “four”)`
   },
   noCampaign: {
     name: 'noCampaign',
