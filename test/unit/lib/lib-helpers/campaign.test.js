895182e2483cc82b9c504b1966adbb2d25214ded
Aaron Schachter <aaronschachter@gmail.com>
2021-01-21 11:05:20 -0800

    Removes campaign helpers for webSignupConfirmations

--- test/unit/lib/lib-helpers/campaign.test.js
+++ test/unit/lib/lib-helpers/campaign.test.js
@@ -6,11 +6,7 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
-const graphql = require('../../../../lib/graphql');
-const helpers = require('../../../../lib/helpers');
-const stubs = require('../../../helpers/stubs');
 const campaignFactory = require('../../../helpers/factories/campaign');
-const webSignupConfirmationFactory = require('../../../helpers/factories/webSignupConfirmation');
 
 chai.should();
 chai.use(sinonChai);
@@ -25,57 +21,6 @@ test.afterEach(() => {
   sandbox.restore();
 });
 
-// fetchWebSignupConfirmations
-test('fetchWebSignupConfirmations returns cached graphql.fetchWebSignupConfirmations result', async () => {
-  const data = [123, 345];
-  sandbox.spy(campaignHelper, 'webSignupConfirmationTransformer');
-  sandbox.stub(graphql, 'fetchWebSignupConfirmations')
-    .returns(Promise.resolve(data));
-  sandbox.stub(helpers.cache.webSignupConfirmations, 'set')
-    .returns(Promise.resolve(data));
-
-  await campaignHelper.fetchWebSignupConfirmations();
-  campaignHelper.webSignupConfirmationTransformer.should.have.been.called;
-  helpers.cache.webSignupConfirmations.set.should.have.been.called;
-});
-
-test('fetchWebSignupConfirmations throws if graphql.fetchWebSignupConfirmations fails', async (t) => {
-  const data = [123, 345];
-  sandbox.stub(graphql, 'fetchWebSignupConfirmations')
-    .returns(Promise.reject(stubs.getError()));
-  sandbox.stub(helpers.cache.webSignupConfirmations, 'set')
-    .returns(Promise.resolve(data));
-
-  await t.throwsAsync(() => campaignHelper.fetchWebSignupConfirmations());
-  helpers.cache.webSignupConfirmations.set.should.not.have.been.called;
-});
-
-// getWebSignupConfirmationByCampaignId
-test('getWebSignupConfirmationByCampaignId returns webSignupConfirmation if fetchWebSignupConfirmations has a webSignupConfirmation with given campaign id', async () => {
-  const campaign = campaignFactory.getValidCampaign();
-  const firstStub = webSignupConfirmationFactory.getValidWebSignupConfirmation(campaign);
-  const secondStub = webSignupConfirmationFactory
-    .getValidWebSignupConfirmation(campaignFactory.getValidCampaign());
-  sandbox.stub(campaignHelper, 'getWebSignupConfirmations')
-    .returns(Promise.resolve([firstStub, secondStub]));
-
-  const result = await campaignHelper.getWebSignupConfirmationByCampaignId(campaign.id);
-  result.should.deep.equal(firstStub);
-});
-
-test('getWebSignupConfirmationByCampaignId returns null if fetchWebSignupConfirmations does not have a webSignupConfirmation with given campaign id', async (t) => {
-  const firstStub = webSignupConfirmationFactory
-    .getValidWebSignupConfirmation(campaignFactory.getValidCampaign());
-  const secondStub = webSignupConfirmationFactory
-    .getValidWebSignupConfirmation(campaignFactory.getValidCampaign());
-  sandbox.stub(campaignHelper, 'getWebSignupConfirmations')
-    .returns(Promise.resolve([firstStub, secondStub]));
-  const campaign = campaignFactory.getValidCampaign();
-
-  const result = await campaignHelper.getWebSignupConfirmationByCampaignId(campaign.id);
-  t.falsy(result);
-});
-
 // isClosedCampaign
 test('isClosedCampaign should return false when campaign.endDate undefined', (t) => {
   t.falsy(campaignHelper.isClosedCampaign(campaignFactory.getValidCampaign()));
@@ -87,6 +32,8 @@ test('isClosedCampaign should return true when campaign.endDate has past', (t) =
 
 test('isClosedCampaign should return false when campaign.endDate is in the future', (t) => {
   const campaign = campaignFactory.getValidClosedCampaign();
+
   campaign.endDate = '2088-07-19T00:00:00Z';
+
   t.falsy(campaignHelper.isClosedCampaign(campaign));
 });
