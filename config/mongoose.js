b385564371bc4c74cc46bd60a57ecb3a6ea2770c
Aaron Schachter <aaronschachter@gmail.com>
2021-01-26 10:43:33 -0800

    Update mongoose to v5 (#550)
    * Updates Mongoose to v5
    
    * Removes deleted useMongoClient option https://mongoosejs.com/docs/migrating_to_5.html#connection-logic
    
    * Fixes deprecation warnings
    
    * removes deprecation warning for collection.ensureIndex
    
    * Updates documentation links

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,13 +6,14 @@ const Promise = require('bluebird');
 const logger = require('../lib/logger');
 
 mongoose.Promise = Promise;
+
 /**
- * @see http://mongoosejs.com/docs/4.x/docs/index.html
- * @see http://mongoosejs.com/docs/4.x/docs/api.html#index_Mongoose-connect
+ * @see https://mongoosejs.com/docs/index.html
  */
 module.exports = url => mongoose.connect(url, {
-  // http://mongoosejs.com/docs/connections.html#use-mongo-client
-  useMongoClient: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).catch((error) => {
   logger.error(error.message);
   // We can't connect to the MongoDB, kill the process immediately so it can restart
